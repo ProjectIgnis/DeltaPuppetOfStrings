c60b7a8bf0fafdd08897670a1394ee68f0dcd0ad
Hatter <sanity.sierota@protonmail.com>
2022-09-16 18:48:40 +0000

    fix "Catalyst Field"
    condition function had wrong parameters

---
+++ script/official/c65959844.lua
@@ -0,0 +1,75 @@
+--化合電界
+--Catalyst Field
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--1 Level 5 or higher Gemini monster can be Normal Summoned without tribute
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SUMMON_PROC)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_HAND,0)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.ntcon)
+	e2:SetTarget(aux.FieldSummonProcTg(s.nttg))
+	c:RegisterEffect(e2)
+	--Can Normal Summon 1 additional Gemini monster
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(LOCATION_HAND|LOCATION_MZONE,0)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_GEMINI))
+	c:RegisterEffect(e3)
+	--Destroy 1 opponent card
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_REMOVE+CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetCountLimit(1)
+	e4:SetTarget(s.destg)
+	e4:SetOperation(s.desop)
+	c:RegisterEffect(e4)
+end
+s.listed_card_types={TYPE_GEMINI}
+function s.ntcon(e,c,minc)
+	if c==nil then return true end
+	return minc==0 and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+end
+function s.nttg(e,c)
+	return c:IsLevelAbove(5) and c:IsType(TYPE_GEMINI)
+end
+function s.rmfilter(c)
+	return c:IsGeminiStatus() and c:IsAbleToRemove()
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local rc=Duel.SelectMatchingCard(tp,s.rmfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	if not rc then return end
+	local reset=RESET_PHASE+PHASE_END+RESET_OPPO_TURN
+	if aux.RemoveUntil(rc,nil,REASON_EFFECT,PHASE_END,id,e,tp,aux.DefaultFieldReturnOp,s.retcon,reset) then
+		local tc=Duel.GetFirstTarget()
+		if not tc:IsRelateToEffect(e) then return end
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
+function s.retcon(ag,e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsTurnPlayer(1-tp)
+end

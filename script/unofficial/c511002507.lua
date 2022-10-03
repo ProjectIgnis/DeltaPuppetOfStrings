8a1af40cc1febddc4b6ce267cb99a66df2927b93
Naim <naimsantos45@gmail.com>
2022-10-03 20:50:06 +0000

    Update "Suijin (Anime)"
    It was causing script errors due to the SelectUnselectGroup function receiving fewer parameter than it needs

---
+++ script/unofficial/c511002507.lua
@@ -0,0 +1,84 @@
+--水魔神－スーガ (Anime)
+--Suijin (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Place  itself in the Spell/Trap Zone
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.settg)
+	e1:SetOperation(s.setop)
+	c:RegisterEffect(e1)
+	--Negate the attack of an opponent's monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+	--Changet its ATK
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.atkcon)
+	e3:SetTarget(s.atktg)
+	e3:SetOperation(s.atkop)
+	c:RegisterEffect(e3)
+end
+s.listed_names={62340868,25955164}
+--place in Szone
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsLocation(LOCATION_HAND) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsImmuneToEffect(e) or not c:IsLocation(LOCATION_HAND) then return end
+	if not Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true) then return end
+end
+--negate attack
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateAttack()
+end
+--atk change
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp and Duel.GetAttacker()==e:GetHandler()
+end
+function s.atkfilter(c,code1,code2)
+	return (c:IsCode(code1) or c:IsCode(code2)) and c:IsMonster()
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetFlagEffect(id)==0
+		and (Duel.IsExistingMatchingCard(s.atkfilter,tp,LOCATION_MZONE,0,1,nil,62340868,25955164)) end
+	e:GetHandler():RegisterFlagEffect(id,RESET_CHAIN,0,1)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_MZONE,0,nil,62340868,25955164)
+	local sg=aux.SelectUnselectGroup(g,e,tp,1,2,aux.dncheck,1,tp,HINTMSG_TARGET)
+	if #sg>0 then
+		local atk=(c:GetAttack()+(sg:GetSum(Card.GetAttack)))/2
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetReset(RESET_PHASE+PHASE_DAMAGE_CAL)
+		e1:SetValue(atk)
+		c:RegisterEffect(e1)
+		for tc in sg:Iter() do
+			local e0=Effect.CreateEffect(c)
+			e0:SetType(EFFECT_TYPE_SINGLE)
+			e0:SetCode(EFFECT_CANNOT_ATTACK)
+			e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e0:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e0)
+		end
+	end
+end

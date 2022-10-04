aa33814c1a98b2ee55ff42b133271e7fc0d4b46d
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-10-04 10:51:54 +0000

    "Fusion Tag" + "Amazoness Kaiserin" fix
    - Amazoness Kaiserin: Now properly checks the materials' names when used for the summon with "Card.IsSummonCode" rather than the materials actual card name.
    - Fusion Tag: Its name changing effect can now also be applied for anything that looks for "SUMMON_TYPE_FUSION" specifically rather than only "MATERIAL_FUSION".

---
+++ script/official/c59432181.lua
@@ -0,0 +1,51 @@
+--融合識別
+--Fusion Tag
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.filter(c,tp)
+	return c:IsFaceup() and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_EXTRA,0,1,nil,c,tp)
+end
+function s.cfilter(c,tc,tp)
+	return c:IsType(TYPE_FUSION) and not c:IsCode(tc:GetCode(nil,SUMMON_TYPE_FUSION,tp))
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not tc:IsRelateToEffect(e) or tc:IsFacedown() then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local cg=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_EXTRA,0,1,1,nil,tc,tp)
+	if #cg==0 then return end
+	Duel.ConfirmCards(1-tp,cg)
+	local code1,code2=cg:GetFirst():GetOriginalCodeRule()
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_ADD_CODE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e1:SetValue(code1)
+	e1:SetOperation(s.chngcon)
+	tc:RegisterEffect(e1)
+	if code2 then
+		local e2=e1:Clone()
+		e2:SetValue(code2)
+		tc:RegisterEffect(e2)
+	end
+end
+function s.chngcon(scard,sumtype,tp)
+	return (sumtype&MATERIAL_FUSION)~=0 or (sumtype&SUMMON_TYPE_FUSION)~=0
+end

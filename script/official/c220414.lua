ca353bc61781165586cb188756a44f55b84689a9
Hatter <sanity.sierota@protonmail.com>
2022-10-04 20:09:19 +0000

    fix "Performapal Turn Trooper"
    - players should be able to select which monsters to return if they don't have enough zones
    - cards should still be the same copy to be returned
    - modernize/polish script

---
+++ script/official/c220414.lua
@@ -0,0 +1,74 @@
+--EMターントルーパー
+--Performapal Turntrooper
+--scripted by pyrQ
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableCounterPermit(0x14a)
+	c:SetCounterLimit(0x14a,2)
+	--Place 1 counter on this card
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_COUNTER)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.countercon)
+	e1:SetOperation(s.counterop)
+	c:RegisterEffect(e1)
+	--Negate attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.atkcon)
+	e2:SetOperation(function() Duel.NegateAttack() end)
+	c:RegisterEffect(e2)
+	--Banish all monsters
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(function(e) return e:GetHandler():GetCounter(0x14a)>=2 end)
+	e3:SetCost(s.rmcost)
+	e3:SetTarget(s.rmtg)
+	e3:SetOperation(s.rmop)
+	c:RegisterEffect(e3)
+end
+function s.countercon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsTurnPlayer(tp) and e:GetHandler():GetCounter(0x14a)<2
+end
+function s.counterop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		c:AddCounter(0x14a,1)
+	end
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker():IsControler(1-tp) and e:GetHandler():GetCounter(0x14a)>=1
+end
+function s.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsReleasable() end
+	Duel.Release(c,REASON_COST)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,LOCATION_MZONE,LOCATION_MZONE,c)
+	if chk==0 then return #g>0 end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,tp,LOCATION_MZONE)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	aux.RemoveUntil(g,nil,REASON_EFFECT,PHASE_END,id,e,tp,aux.DefaultFieldReturnOp,s.retcon,RESET_PHASE|PHASE_END|RESET_OPPO_TURN,2)
+end
+function s.retcon(ag,e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsTurnPlayer(tp) then return false end
+	local fid,t=e:GetLabel()
+	if t==1 then return true end
+	e:SetLabel(fid,1)
+	return false
+end

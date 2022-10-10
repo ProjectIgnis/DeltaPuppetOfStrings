1e4a3322d66d4c42b285936c52637b3a4b913beb
Naim <naimsantos45@gmail.com>
2022-10-10 16:48:46 +0000

    "World Legacy Monstrosity" fix
    It was calling an incorrect filter name

---
+++ script/official/c14604710.lua
@@ -0,0 +1,118 @@
+--星遺物の胎導
+--World Legacy Monstrosity
+--Scripted by ahtelel and Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special summon 2 level 9 monsters from deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target)
+	c:RegisterEffect(e1)
+end
+function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter2(chkc,e,tp) end
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsExistingTarget(s.spfilter2,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.spfilter2,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_DECK)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc) end
+	local b1=s.sptg1(e,tp,eg,ep,ev,re,r,rp,0)
+	local b2=s.sptg2(e,tp,eg,ep,ev,re,r,rp,0)
+	if chk==0 then return b1 or b2 end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,0)},
+		{b2,aux.Stringid(id,1)})
+	if op==1 then
+		e:SetProperty(0)
+		e:SetOperation(s.spop1)
+		s.sptg1(e,tp,eg,ep,ev,re,r,rp,1)
+	else
+		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+		e:SetOperation(s.spop2)
+		s.sptg2(e,tp,eg,ep,ev,re,r,rp,1)
+	end
+end
+function s.spfilter1(c,e,tp)
+	return c:IsLevel(9) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.spfilter2(c,e,tp)
+	if not c:IsFaceup() or not c:IsLevel(9) then return false end
+	local g=Duel.GetMatchingGroup(s.spfilter3,tp,LOCATION_DECK,0,nil,e,tp,c)
+	return g:GetClassCount(Card.GetCode)>1
+end
+function s.spfilter3(c,e,tp,tc)
+	return c:IsLevel(9) and c:GetOriginalRace()~=tc:GetOriginalRace() and c:GetOriginalAttribute()~=tc:GetOriginalAttribute()
+		and not c:IsCode(tc:GetCode()) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if not tc:IsRelateToEffect(e) or ft<2 or Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+	local g=Duel.GetMatchingGroup(s.spfilter3,tp,LOCATION_DECK,0,nil,e,tp,tc)
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,aux.dncheck,1,tp,HINTMSG_SPSUMMON)
+	if sg and #sg==2 then
+		local fid=c:GetFieldID()
+		for sc in sg:Iter() do
+			Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP)
+			--Cannot attack
+			local e1=Effect.CreateEffect(c)
+			e1:SetDescription(3206)
+			e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			sc:RegisterEffect(e1,true)
+			sc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1,fid)
+		end
+		Duel.SpecialSummonComplete()
+		sg:KeepAlive()
+		--Destroy them during end phase
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_PHASE+PHASE_END)
+		e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e2:SetCountLimit(1)
+		e2:SetLabel(fid)
+		e2:SetLabelObject(sg)
+		e2:SetCondition(s.descon)
+		e2:SetOperation(s.desop)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
+function s.desfilter(c,fid)
+	return c:GetFlagEffectLabel(id)==fid
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	if not g:IsExists(s.desfilter,1,nil,e:GetLabel()) then
+		g:DeleteGroup()
+		e:Reset()
+		return false
+	else return true end
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=e:GetLabelObject()
+	local tg=g:Filter(s.desfilter,nil,e:GetLabel())
+	Duel.Destroy(tg,REASON_EFFECT)
+end

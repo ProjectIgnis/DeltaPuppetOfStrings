c0e3fb06505dd30e9f7fe240a51dee5eb8ee02db
Naim <naimsantos45@gmail.com>
2022-10-10 21:11:34 +0000

    "Zoodiac Boarbow" fix
    It should not be changed to Defense position if no cards were sent to the GY

---
+++ script/official/c74393852.lua
@@ -0,0 +1,79 @@
+--十二獣ワイルドボウ
+--Zoodiac Boarbow
+local s,id=GetID()
+function s.initial_effect(c)
+	--Xyz Summon Procedure
+	Xyz.AddProcedure(c,nil,4,5,s.ovfilter,aux.Stringid(id,0),99,s.xyzop)
+	c:EnableReviveLimit()
+	--Gain the ATK/DEF of all "Zoodiac" monsters attached to itself
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.atkval)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	e2:SetValue(s.defval)
+	c:RegisterEffect(e2)
+	--Send cards on the field to the GY
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TOGRAVE+CATEGORY_POSITION)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BATTLE_DAMAGE)
+	e3:SetCondition(s.condition)
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.operation)
+	c:RegisterEffect(e3)
+	--Can attack directly
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_DIRECT_ATTACK)
+	c:RegisterEffect(e4)
+end
+s.listed_series={SET_ZOODIAC}
+function s.ovfilter(c,tp,lc)
+	return c:IsFaceup() and c:IsSetCard(SET_ZOODIAC,lc,SUMMON_TYPE_XYZ,tp) and not c:IsSummonCode(lc,SUMMON_TYPE_XYZ,tp,id)
+end
+function s.xyzop(e,tp,chk)
+	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 end
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+	return true
+end
+function s.atkfilter(c)
+	return c:IsSetCard(SET_ZOODIAC) and c:GetAttack()>=0
+end
+function s.atkval(e,c)
+	local g=e:GetHandler():GetOverlayGroup():Filter(s.atkfilter,nil)
+	return g:GetSum(Card.GetAttack)
+end
+function s.deffilter(c)
+	return c:IsSetCard(SET_ZOODIAC) and c:GetDefense()>=0
+end
+function s.defval(e,c)
+	local g=e:GetHandler():GetOverlayGroup():Filter(s.deffilter,nil)
+	return g:GetSum(Card.GetDefense)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return ep==1-tp and e:GetHandler():GetOverlayCount()>=12
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND+LOCATION_ONFIELD)
+	if chk==0 then return #g>0 end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,#g,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND+LOCATION_ONFIELD)
+	if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 then
+		local og=Duel.GetOperatedGroup()
+		if og:FilterCount(Card.IsLocation,0,LOCATION_GRAVE)==0 then return end
+		local c=e:GetHandler()
+		if c:IsRelateToEffect(e) and c:IsFaceup() then
+			Duel.BreakEffect()
+			Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+		end
+	end
+end

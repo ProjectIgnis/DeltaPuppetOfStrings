7c65a9387c19d0b662146bce953a3224dc196cc6
ClaireStanfield <51684310+ClaireStanfield@users.noreply.github.com>
2022-10-16 04:40:13 +0000

    Fix Slifer & Ra (VG) (#800)
    Fix Slifer the Sky Dragon (VG) and The Winged Dragon of Ra (VG)
    
    Co-authored-by: Larry126 <28985622+larry126@users.noreply.github.com>

---
+++ script/unofficial/c110000010.lua
@@ -0,0 +1,163 @@
+--ラーの翼神竜 (TF4)
+--The Winged Dragon of Ra (TF4)
+local s,id=GetID()
+function s.initial_effect(c)
+	--summon with 3 tribute
+	local e1=aux.AddNormalSummonProcedure(c,true,false,3,3)
+	local e2=aux.AddNormalSetProcedure(c)
+	--summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_CANNOT_DISABLE_SUMMON)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	c:RegisterEffect(e3)
+	--summon success
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_SUMMON_SUCCESS)
+	e4:SetOperation(s.sumsuc)
+	c:RegisterEffect(e4)
+	--cannot be target
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetValue(1)
+	c:RegisterEffect(e5)
+	--to grave
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,0))
+	e6:SetCategory(CATEGORY_TOGRAVE)
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCountLimit(1)
+	e6:SetCode(EVENT_PHASE+PHASE_END)
+	e6:SetCondition(s.tgcon)
+	e6:SetTarget(s.tgtg)
+	e6:SetOperation(s.tgop)
+	c:RegisterEffect(e6)
+	--tribute check
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetCode(EFFECT_MATERIAL_CHECK)
+	e7:SetValue(s.valcheck)
+	c:RegisterEffect(e7)
+	--give atk effect only when summon
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_SINGLE)
+	e8:SetCode(EFFECT_SUMMON_COST)
+	e8:SetOperation(s.facechk)
+	e8:SetLabelObject(e7)
+	c:RegisterEffect(e8)
+	--destroy
+	local e9=Effect.CreateEffect(c)
+	e9:SetDescription(aux.Stringid(id,1))
+	e9:SetCategory(CATEGORY_DESTROY)
+	e9:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET)
+	e9:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e9:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e9:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e9:SetCost(s.descost)
+	e9:SetTarget(s.destg)
+	e9:SetOperation(s.desop)
+	c:RegisterEffect(e9)
+	--pay atk/def
+	local e10=Effect.CreateEffect(c)
+	e10:SetDescription(aux.Stringid(id,2))
+	e10:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e10:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e10:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e10:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e10:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e10:SetCost(s.adcost)
+	e10:SetOperation(s.adop)
+	c:RegisterEffect(e10)
+end
+function s.sumsuc(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SetChainLimitTillChainEnd(aux.FALSE)
+end
+function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,e:GetHandler(),1,0,0)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		Duel.SendtoGrave(c,REASON_EFFECT)
+	end
+end
+function s.valcheck(e,c)
+	local g=c:GetMaterial()
+	local tc=g:GetFirst()
+	local atk=0
+	local def=0
+	while tc do
+		local catk=tc:GetTextAttack()
+		local cdef=tc:GetTextDefense()
+		atk=atk+(catk>=0 and catk or 0)
+		def=def+(cdef>=0 and cdef or 0)
+		tc=g:GetNext()
+	end
+	if e:GetLabel()==1 then
+		e:SetLabel(0)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE-RESET_TOFIELD)
+		c:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_SET_DEFENSE)
+		e2:SetValue(def)
+		c:RegisterEffect(e2)
+	end
+end
+function s.facechk(e,tp,eg,ep,ev,re,r,rp)
+	e:GetLabelObject():SetLabel(1)
+end
+function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsAbleToRemove() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT,LOCATION_REMOVED)
+	end
+end
+function s.adcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLP(tp)>1 end
+	local lp=Duel.GetLP(tp)
+	e:SetLabel(lp-1)
+	Duel.PayLPCost(tp,lp-1)
+end
+function s.adop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(e:GetLabel())
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_UPDATE_DEFENSE)
+		c:RegisterEffect(e2)
+	end
+end
---
+++ script/unofficial/c110000012.lua
@@ -0,0 +1,95 @@
+--オシリスの天空竜 (TF6)
+--Slifer the Sky Dragon (TF6)
+local s,id=GetID()
+function s.initial_effect(c)
+	--summon with 3 tribute
+	local e1=aux.AddNormalSummonProcedure(c,true,false,3,3)
+	local e2=aux.AddNormalSetProcedure(c)
+	--summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_CANNOT_DISABLE_SUMMON)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	c:RegisterEffect(e3)
+	--summon success
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_SUMMON_SUCCESS)
+	e4:SetOperation(s.sumsuc)
+	c:RegisterEffect(e4)
+	--cannot be target
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetValue(1)
+	c:RegisterEffect(e5)
+	--to grave
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,0))
+	e6:SetCategory(CATEGORY_TOGRAVE)
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCountLimit(1)
+	e6:SetCode(EVENT_PHASE+PHASE_END)
+	e6:SetCondition(s.tgcon)
+	e6:SetTarget(s.tgtg)
+	e6:SetOperation(s.tgop)
+	c:RegisterEffect(e6)
+	--atk/def
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetCode(EFFECT_SET_ATTACK)
+	e7:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e7:SetRange(LOCATION_MZONE)
+	e7:SetValue(s.adval)
+	c:RegisterEffect(e7)
+	local e8=e7:Clone()
+	e8:SetCode(EFFECT_SET_DEFENSE)
+	c:RegisterEffect(e8)
+	--destroy
+	local e9=Effect.CreateEffect(c)
+	e9:SetDescription(aux.Stringid(id,1))
+	e9:SetCategory(CATEGORY_DESTROY)
+	e9:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e9:SetRange(LOCATION_MZONE)
+	e9:SetCode(EVENT_SUMMON_SUCCESS)
+	e9:SetTarget(s.destg)
+	e9:SetOperation(s.desop)
+	c:RegisterEffect(e9)
+	local e10=e9:Clone()
+	e10:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e10)
+end
+function s.sumsuc(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SetChainLimitTillChainEnd(aux.FALSE)
+end
+function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,e:GetHandler(),1,0,0)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		Duel.SendtoGrave(c,REASON_EFFECT)
+	end
+end
+function s.adval(e,c)
+	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_HAND,0)*1000
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=eg:GetFirst()
+	if chk==0 then return e:GetHandler():IsRelateToEffect(e) and not tc:IsSummonPlayer(tp) and tc:IsFaceup() and tc:IsDefenseBelow(2000) end
+	Duel.SetTargetCard(tc)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsDefenseBelow(2000) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end

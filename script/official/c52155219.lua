ebb83415207cc75b6becc125711c95e4303c20ae
Naim <naimsantos45@gmail.com>
2022-10-17 15:50:55 +0000

    Update "Salamangreat Circle"
    Added Hint when selecting the card + updates to use the Duel.SelectEffect function

---
+++ script/official/c52155219.lua
@@ -0,0 +1,67 @@
+--転生炎獣の炎陣
+--Salamangreat Circle
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	c:RegisterEffect(e1)
+end
+s.listed_series={SET_SALAMANGREAT}
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.immfilter(chkc) end
+	local b1=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
+	local b2=Duel.IsExistingTarget(s.immfilter,tp,LOCATION_MZONE,0,1,nil)
+	if chk==0 then return b1 or b2 end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,0)},
+		{b2,aux.Stringid(id,1)})
+	if op==0 then
+		e:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+		e:SetProperty(0)
+		e:SetOperation(s.thop)
+	else
+		e:SetCategory(0)
+		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+		e:SetOperation(s.immop)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_APPLYTO)
+		Duel.SelectTarget(tp,s.immfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	end
+end
+function s.thfilter(c)
+	return c:IsSetCard(SET_SALAMANGREAT) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.immfilter(c)
+	return c:IsLinkMonster() and c:IsSetCard(SET_SALAMANGREAT) and c:IsReincarnationSummoned()
+end
+function s.immop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		--Unaffected by monster effects
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3101)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EFFECT_IMMUNE_EFFECT)
+		e1:SetValue(s.efilter)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
+function s.efilter(e,te)
+	return te:IsActiveType(TYPE_MONSTER) and te:GetOwner()~=e:GetOwner()
+end

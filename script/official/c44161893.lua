187cb7130fa1b181d7fba96bc909bda4df14256e
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-10-21 19:25:20 +0000

    Various script fixes/updates
    - Boo-Boo Game: Missing the targeting flag, shouldn't be able to target 2 Traps if you only have 1 empty Spell/Trap Zone + small overall polishing.
    - Core of Chaos/Shell of Chaos: Shouldn't be banished if they leave the field while face-down.
    - Karakuri Bonze mdl 9763 "Kunamzan": Should remain a Tuner if its effects are negated after it's Special Summoned.
    - Royal Straight: Shouldn't be able to Special Summon from the GY if the effect of "Necrovalley" is being applied.
    - Salamangreat Zebroid X: Shouldn't check for valid targets since its effect doesn't target, wasn't negating the monsters' effects + minor fixes and general polishing all around.
    - Silver-Winged AXE-Sarry: Should be able to use its effect in the Damage Step.

---
+++ script/official/c44161893.lua
@@ -0,0 +1,104 @@
+--転生炎獣ゼブロイドＸ
+--Salamangreat Zebroid X
+--Anime version scripted by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon from your GY and Xyz Summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY,EFFECT_FLAG2_CHECK_SIMULTANEOUS)
+	e1:SetCode(EVENT_LEAVE_FIELD)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Grant an effect when used as Xyz Material
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_BE_MATERIAL)
+	e2:SetCondition(s.efcon)
+	e2:SetOperation(s.efop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_SALAMANGREAT}
+function s.cfilter(c,tp)
+	return c:GetPreviousTypeOnField()&TYPE_LINK>0 and c:IsPreviousSetCard(SET_SALAMANGREAT) and c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousControler(tp)
+		and c:IsPreviousLocation(LOCATION_MZONE) and c:GetReasonPlayer()==1-tp and c:IsReason(REASON_EFFECT)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return not eg:IsContains(e:GetHandler()) and eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.spfilter(c,e,tp,tc)
+	return c:IsSetCard(SET_SALAMANGREAT) and c:IsLevel(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_EXTRA,0,1,nil,Group.FromCards(c,tc))
+end
+function s.xyzfilter(c,mg)
+	return c:IsSetCard(SET_SALAMANGREAT) and c:IsType(TYPE_XYZ) and c:IsXyzSummonable(nil,mg,2,2)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsPlayerCanSpecialSummonCount(tp,2) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,c,e,tp,c) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,3,tp,LOCATION_GRAVE|LOCATION_EXTRA)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or not Duel.IsPlayerCanSpecialSummonCount(tp,2) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
+	local c=e:GetHandler()
+	if not (c:IsRelateToEffect(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,c,e,tp,c)
+	if #sg==0 then return end
+	sg:AddCard(c)
+	for sc in sg:Iter() do
+		if Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP) then
+			--Negate their effects
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e1:SetCode(EFFECT_DISABLE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			sc:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_DISABLE_EFFECT)
+			e2:SetValue(RESET_TURN_SET)
+			sc:RegisterEffect(e2)
+		end
+	end
+	if Duel.SpecialSummonComplete()==0 then return end
+	Duel.BreakEffect()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local xyz=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_EXTRA,0,1,1,nil,sg):GetFirst()
+	if xyz then
+		Duel.XyzSummon(tp,xyz,nil,sg)
+	end
+end
+function s.efcon(e,tp,eg,ep,ev,re,r,rp)
+	return (r&REASON_XYZ)>0 and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.efop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	--Increase ATK
+	local e1=Effect.CreateEffect(rc)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(function(_,c) return c:GetOverlayCount()*300 end)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	rc:RegisterEffect(e1,true)
+	if not rc:IsType(TYPE_EFFECT) then
+		--Treated as an Effect Monster
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_ADD_TYPE)
+		e2:SetValue(TYPE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		rc:RegisterEffect(e2,true)
+	end
+end

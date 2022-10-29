3ace57d382ec97f4c8ddf8cc9af4eb4787c424c6
Edoardo Lolletti <edoardo762@gmail.com>
2022-10-29 20:19:01 +0000

    Forbidden Trapezohedron script cleanup

---
+++ script/official/c49033797.lua
@@ -0,0 +1,61 @@
+--禁断のトラペゾヘドロン
+--Forbidden Trapezohedron
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={SET_OUTER_ENTITY,SET_ELDER_ENTITY,SET_OLD_ENTITY}
+function s.filter(c,e,tp,cat)
+	return c:IsSetCard(cat) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local flag=Duel.GetFieldGroup(tp,LOCATION_MZONE,0):GetBitwiseOr(function(c)return c:GetType()&(TYPE_FUSION|TYPE_SYNCHRO|TYPE_XYZ)end)
+		e:SetLabel(flag)
+		if flag==(TYPE_FUSION|TYPE_SYNCHRO) then
+			return e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_EXTRA,0,1,nil,e,tp,SET_OUTER_ENTITY)
+		elseif flag==(TYPE_SYNCHRO|TYPE_XYZ) then
+			return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_EXTRA,0,1,nil,e,tp,SET_ELDER_ENTITY)
+		elseif flag==(TYPE_FUSION|TYPE_XYZ) then
+			return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_EXTRA,0,1,nil,e,tp,SET_OLD_ENTITY)
+		else
+			return false
+		end
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local flag=e:GetLabel()
+	if flag==(TYPE_FUSION|TYPE_SYNCHRO) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,SET_OUTER_ENTITY):GetFirst()
+		if sc then
+			Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+			local c=e:GetHandler()
+			if c:IsRelateToEffect(e) then
+				c:CancelToGrave()
+				Duel.Overlay(sc,c)
+			end
+		end
+	elseif flag==(TYPE_SYNCHRO|TYPE_XYZ) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,SET_ELDER_ENTITY)
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	elseif flag==(TYPE_FUSION|TYPE_XYZ) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,SET_OLD_ENTITY)
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end

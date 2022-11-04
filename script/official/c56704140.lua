bb48a9985c78ed5b9220fee4d592babcc9929864
pyrQ <30827955+pyrQ@users.noreply.github.com>
2022-11-04 10:44:45 +0000

    Some card fixes
    - Dark Armed, the Dragon of Annihilation: General polish and fixed its interaction with "Spirit Elimination".
    - Geomancer of the Ice Barrier: Added a hint with the declared Attribute on the card.
    - Spellbreaker of the Ice Barrier: The effect should remain applied even if its effects are negated after the effect has already resolved.

---
+++ script/official/c56704140.lua
@@ -0,0 +1,39 @@
+--氷結界の風水師
+--Geomancer of the Ice Barrier
+local s,id=GetID()
+function s.initial_effect(c)
+	--Make this card untargetable for attacks
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(aux.IceBarrierDiscardCost(nil,true))
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local attr=Duel.AnnounceAttribute(tp,1,ATTRIBUTE_ALL)
+	e:SetLabel(attr)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+	local attr=e:GetLabel()
+	c:SetHint(CHINT_ATTRIBUTE,attr)
+	--Cannot be targeted for attacks
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+	e1:SetLabel(attr)
+	e1:SetValue(s.tgval)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	c:RegisterEffect(e1)
+end
+function s.tgval(e,c)
+	return c:IsAttribute(e:GetLabel()) and not c:IsImmuneToEffect(e)
+end
---
+++ script/official/c73061465.lua
@@ -0,0 +1,35 @@
+--氷結界の封魔団
+--Spellbreaker of the Ice Barrier
+local s,id=GetID()
+function s.initial_effect(c)
+	--Apply activation restriction
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(aux.IceBarrierDiscardCost(s.cfilter,false))
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_series={SET_ICE_BARRIER}
+function s.cfilter(c)
+	return c:IsSetCard(SET_ICE_BARRIER) and c:IsMonster() and c:IsAbleToGraveAsCost()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+	--Neither player can activate Spell Cards
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(1,1)
+	e1:SetValue(s.tgval)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,3)
+	c:RegisterEffect(e1)
+end
+function s.tgval(e,re,rp)
+	return re:IsSpellEffect() and re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end

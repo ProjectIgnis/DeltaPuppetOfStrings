e1859eafb14bff2899ee06917c87f64b35a57fc1
Naim <naimsantos45@gmail.com>
2022-11-13 14:38:11 +0000

    Fix "Simorgh, Bird of Sovereignty"
    According to rulings, zones that are made 'unusuable' by effects like "Mystical Cards of Light" are still considered when evaluating the level of the monster to be summoned

---
+++ script/official/c72330894.lua
@@ -0,0 +1,95 @@
+--王神鳥シムルグ
+--Simorgh, Bird of Sovereignty
+--Scripted by AlphaKretin
+local s,id=GetID()
+function s.initial_effect(c)
+	--Link Summon
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,nil,2,3,s.lcheck)
+	--Cannot be Link Material
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--Opponent cannot target Winged-Beast monsters
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(s.indtg)
+	e2:SetValue(aux.tgoval)
+	c:RegisterEffect(e2)
+	--Destruction replacement
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetTarget(s.desreptg)
+	e3:SetOperation(s.desrepop)
+	c:RegisterEffect(e3)
+	--Special Summon 1 Winged-Beast monster from the Deck
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,id)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
+	c:RegisterEffect(e4)
+end
+s.listed_series={SET_SIMORGH}
+function s.lcheck(g,lc,sumtype,tp)
+	return g:IsExists(Card.IsRace,1,nil,RACE_WINGEDBEAST,lc,sumtype,tp)
+end
+function s.indtg(e,c)
+	local oc=e:GetHandler()
+	return c==oc or (c:IsRace(RACE_WINGEDBEAST) and oc:GetLinkedGroup():IsContains(c))
+end
+function s.repfilter(c,e)
+	return c:IsFaceup() and c:IsSetCard(SET_SIMORGH)
+		and c:IsDestructable(e) and not c:IsStatus(STATUS_DESTROY_CONFIRMED)
+end
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsReason(REASON_BATTLE) and c:IsOnField() and c:IsFaceup()
+		and Duel.IsExistingMatchingCard(s.repfilter,tp,LOCATION_ONFIELD,0,1,c,e) end
+	if Duel.SelectEffectYesNo(tp,c,96) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
+		local g=Duel.SelectMatchingCard(tp,s.repfilter,tp,LOCATION_ONFIELD,0,1,1,c,e)
+		Duel.SetTargetCard(g)
+		g:GetFirst():SetStatus(STATUS_DESTROY_CONFIRMED,true)
+		return true
+	else return false end
+end
+function s.desrepop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	g:GetFirst():SetStatus(STATUS_DESTROY_CONFIRMED,false)
+	Duel.Destroy(g,REASON_EFFECT+REASON_REPLACE)
+end
+function s.sequence(c)
+	return c:GetSequence()<5
+end
+function s.spfilter(c,e,tp,lv)
+	return c:IsRace(RACE_WINGEDBEAST) and c:IsLevelBelow(lv) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=5*2-Duel.GetMatchingGroupCount(s.sequence,0,LOCATION_SZONE,LOCATION_SZONE,nil)
+	if chk==0 then return ct>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and 
+		Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp,ct) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local ct=5*2-Duel.GetMatchingGroupCount(s.sequence,0,LOCATION_SZONE,LOCATION_SZONE,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp,ct)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end

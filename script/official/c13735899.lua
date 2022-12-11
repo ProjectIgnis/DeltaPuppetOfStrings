cd1b38dd7d7d8d8d093c7bd42805329d9aaabbb9
Naim <naimsantos45@gmail.com>
2022-12-11 16:54:49 +0000

    Update "Mysterion the Dragon Crown"
    Improve the script to handle the scenarios where monsters are summoned in different timings in the chain, allowing the player to target monsters from a single group

---
+++ script/official/c13735899.lua
@@ -0,0 +1,99 @@
+--ミュステリオンの竜冠
+--Mysterion the Dragon Crown
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion Summon
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,RACE_SPELLCASTER),aux.FilterBoolFunctionEx(Card.IsRace,RACE_DRAGON))
+	--Cannot be used as Fusion Material
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+	e0:SetValue(1)
+	c:RegisterEffect(e0)
+	--Decreak ATK by 100 per banished card
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(function(e,cc) return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_REMOVED,0)*-100 end)
+	c:RegisterEffect(e1)
+	--Banish monsters
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_CUSTOM+id)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.rmtg)
+	e2:SetOperation(s.rmop)
+	c:RegisterEffect(e2)
+	local g=Group.CreateGroup()
+	g:KeepAlive()
+	e2:SetLabelObject(g)
+	--Register summons
+	local e2a=Effect.CreateEffect(c)
+	e2a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2a:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2a:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2a:SetRange(LOCATION_MZONE)
+	e2a:SetLabelObject(e2)
+	e2a:SetOperation(s.regop)
+	c:RegisterEffect(e2a)
+end
+function s.tgfilter(c,rc,e)
+	return c:IsLocation(LOCATION_MZONE) and (rc==c or (c:IsFaceup() and rc:IsOriginalRace(c:GetOriginalRace())))
+		and c:IsAbleToRemove() and (not e or c:IsCanBeEffectTarget(e))
+end
+function s.tgfilter2(c,e)
+	return c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:IsAbleToRemove() and (not e or c:IsCanBeEffectTarget(e))
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local rc=re:GetHandler()
+	local g=e:GetLabelObject():Filter(s.tgfilter2,nil,e)
+	if chkc then return g:IsContains(chkc) and s.tgfilter(chkc,nil) end
+	if chk==0 then return #g>0 end
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+	local tc=nil
+	if #g==1 then
+		tc=g:GetFirst()
+		Duel.SetTargetCard(tc)
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		tc=g:Select(tp,1,1,nil)
+		Duel.SetTargetCard(tc)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,tc,1,0,0)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		local g=Group.FromCards(tc)
+		if tc:IsFaceup() then
+			g=g+Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsOriginalRace,tc:GetOriginalRace()),tp,LOCATION_MZONE,LOCATION_MZONE,tc)
+		end
+		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	end
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(tp,id)>0 then return end
+	local rc=re:GetHandler()
+	if not (re and re:IsActivated() and re:IsActiveType(TYPE_MONSTER) and rc) then return end
+	local c=e:GetHandler()
+	local tg=eg:Filter(s.tgfilter,nil,rc)
+	if #tg>0 then
+		for tc in tg:Iter() do
+			tc:RegisterFlagEffect(id,RESET_CHAIN,0,1)
+		end
+		local g=e:GetLabelObject():GetLabelObject()
+		if Duel.GetCurrentChain()==0 then g:Clear() end
+		g:Merge(tg)
+		g:Remove(function(c) return c:GetFlagEffect(id)==0 end,nil)
+		e:GetLabelObject():SetLabelObject(g)
+		Duel.RaiseSingleEvent(e:GetHandler(),EVENT_CUSTOM+id,e,0,tp,tp,0)
+	end
+end

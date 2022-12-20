1ae31b799d433b42422a9caf93891fa865a5b592
Naim <naimsantos45@gmail.com>
2022-12-20 22:24:51 +0000

    Update "Spool Code"
    Replace functions (AnnounceNumber by AnnounceNumberRange)

---
+++ script/official/c2625939.lua
@@ -0,0 +1,45 @@
+--スプール・コード
+--Spool Code
+--Scripted by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={2625940}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker():IsControler(1-tp) and Duel.GetAttackTarget()==nil
+		and Duel.IsExistingMatchingCard(Card.IsRace,tp,LOCATION_GRAVE,0,3,nil,RACE_CYBERSE)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.NegateAttack() then return end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft==0 or not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,0,0,1,RACE_CYBERSE,ATTRIBUTE_LIGHT,POS_FACEUP_DEFENSE)
+		or not Duel.SelectYesNo(tp,aux.Stringid(id,1)) then return end
+	ft=math.min(ft,3)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	if ft>1 then
+		ft=Duel.AnnounceNumberRange(tp,1,ct)
+	end
+	Duel.BreakEffect()
+	for i=1,ft do
+		local token=Duel.CreateToken(tp,id+1)
+		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+		--Cannot be tributed for a tribute summon
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3304)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UNRELEASABLE_SUM)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(1)
+		token:RegisterEffect(e1,true)
+	end
+	Duel.SpecialSummonComplete()
+end

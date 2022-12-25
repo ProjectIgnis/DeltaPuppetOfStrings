8fe98e26214adea3cddb8690664e5ec865f8a89f
Naim <naimsantos45@gmail.com>
2022-12-25 13:51:53 +0000

    Update "Machine Angel Absolute Ritual"
    Fixed a bug where Necrovalley would negate it in all scenarios

---
+++ script/official/c11398951.lua
@@ -0,0 +1,31 @@
+--機械天使の絶対儀式
+--Machine Angel Absolute Ritual
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	Ritual.AddProcEqual{handler=c,filter=s.ritualfil,extrafil=s.extrafil,extraop=s.extraop,extratg=s.extratg}
+end
+s.listed_series={SET_CYBER_ANGEL}
+function s.ritualfil(c)
+	return c:IsSetCard(SET_CYBER_ANGEL) and c:IsRitualMonster()
+end
+function s.mfilter(c)
+	return c:HasLevel() and c:IsRace(RACE_WARRIOR+RACE_FAIRY) and c:IsAbleToDeck()
+end
+function s.extrafil(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_GRAVE,0,nil)
+	else
+		return Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_GRAVE,0,nil):Filter(aux.nvfilter,nil)
+	end
+end
+function s.extraop(mg,e,tp,eg,ep,ev,re,r,rp)
+	local mat2=mg:Filter(Card.IsLocation,nil,LOCATION_GRAVE):Filter(Card.IsRace,nil,RACE_WARRIOR+RACE_FAIRY)
+	mg:Sub(mat2)
+	Duel.ReleaseRitualMaterial(mg)
+	Duel.SendtoDeck(mat2,nil,2,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+end

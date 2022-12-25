1206a86ac76ed930d5922216bd2d3b0804b133cc
Naim <naimsantos45@gmail.com>
2022-12-25 13:38:29 +0000

    Update "Altergeist Marionetter" to match rulings
    - It should not be able to Special Summon the monster from the GY if Necrovalley is applying
    -Update to match Protocol vs Skill Drain ruling (it should summon the monster even if the sent card is Protocol)

---
+++ script/official/c53143898.lua
@@ -0,0 +1,73 @@
+--オルターガイスト・マリオネッター
+--Altergeist Marionetter
+local s,id=GetID()
+function s.initial_effect(c)
+	--Set 1 "Altergeist" Trap from the Deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(s.settg)
+	e1:SetOperation(s.setop)
+	c:RegisterEffect(e1)
+	--Send 1 face-up "Altergeist" card to the GY and Special Summon 1 "Altergeist" monster from the GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_ALTERGEIST}
+function s.setfilter(c)
+	return c:IsSetCard(SET_ALTERGEIST) and c:IsTrap() and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local tc=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.SSet(tp,tc)
+		if tc.act_turn then
+			local e0=Effect.CreateEffect(tc)
+			e0:SetType(EFFECT_TYPE_SINGLE)
+			e0:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+			e0:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+			e0:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e0)
+		end
+	end
+end
+function s.tgfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(SET_ALTERGEIST) and c:IsAbleToGrave() and Duel.GetMZoneCount(tp,c)>0 
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(SET_ALTERGEIST) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_ONFIELD,0,1,nil,tp)
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g1=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_ONFIELD,0,1,1,nil,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g2=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g1,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g2,1,0,0)
+	e:SetLabelObject(g1:GetFirst())
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc1,tc2=Duel.GetFirstTarget()
+	if tc1~=e:GetLabelObject() then tc1,tc2=tc2,tc1 end
+	if tc1:IsRelateToEffect(e) and Duel.SendtoGrave(tc1,REASON_EFFECT)>0 and tc1:IsLocation(LOCATION_GRAVE) 
+		and tc2:IsRelateToEffect(e) and aux.nvfilter(tc2) then
+		Duel.SpecialSummon(tc2,0,tp,tp,false,false,POS_FACEUP)
+	end
+end

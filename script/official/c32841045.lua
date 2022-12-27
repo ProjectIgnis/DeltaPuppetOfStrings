8645effd1a94d5bfa9455c9fe794dfadb5d0fb5e
Edoardo Lolletti <edoardo762@gmail.com>
2022-12-27 12:14:29 +0000

    Magical Musketeer Caspar fix wrong string

---
+++ script/official/c32841045.lua
@@ -0,0 +1,86 @@
+--魔弾の射手 カスパール
+--Magical Musketeer Caspar
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate from hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_QP_ACT_IN_NTPHAND)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x108))
+	e1:SetTargetRange(LOCATION_HAND,0)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+	c:RegisterEffect(e2)
+	--to hand
+	local g=Group.CreateGroup()
+	g:KeepAlive()
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetCode(EVENT_CHAIN_SOLVING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetLabelObject(g)
+	e3:SetOperation(s.regop)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,id)
+	e4:SetCondition(s.thcon)
+	e4:SetLabelObject(g)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0x108}
+function s.regop(e)
+	local c=e:GetHandler()
+	local flageff={c:GetFlagEffectLabel(1)}
+	if flageff[1]==nil or c:GetFlagEffect(2)>0 then return end
+	c:RegisterFlagEffect(2,RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_CHAIN,0,1)
+	local g=e:GetLabelObject()
+	g:Clear()
+	for _,i in ipairs(flageff) do
+		g:AddCard(Duel.GetCardFromCardID(i))
+	end
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local p,loc,seq=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_CONTROLER,CHAININFO_TRIGGERING_LOCATION,CHAININFO_TRIGGERING_SEQUENCE)
+	if not (re:IsHasType(EFFECT_TYPE_ACTIVATE) and c:IsColumn(seq,p,LOCATION_SZONE)) then return false end
+	local flageff=c:GetFlagEffectLabel(1)
+	c:RegisterFlagEffect(1,RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET+RESET_CHAIN,0,1,re:GetHandler():GetCardID())
+	if flageff==nil then e:GetLabelObject():Clear() end
+	return flageff==nil
+end
+function s.thfilter(c,...)
+	return c:IsSetCard(0x108) and not c:IsCode(...) and c:IsAbleToHand()
+end
+function s.chk(c,tp,e)
+	return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,c,c:GetCode())
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=e:GetLabelObject()
+	if chk==0 then return g:IsExists(s.chk,1,nil,tp,e) end
+	if #g>1 then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
+		e:SetLabel(g:Select(tp,1,1,nil):GetFirst():GetCardID())
+	else
+		e:SetLabel(g:GetFirst():GetCardID())
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetCardFromCardID(e:GetLabel())
+	if not tc then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,e:GetLabelObject(),tc:GetCode())
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end

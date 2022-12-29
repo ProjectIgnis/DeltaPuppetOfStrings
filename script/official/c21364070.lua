ea95ebefa2d1a950673c6aba0004163c87e70c0a
Naim <naimsantos45@gmail.com>
2022-12-29 11:53:19 +0000

    Fix "Mayosenju Hitot"
    Fixed a bug where the ATK gained would be lost if the monster had its effects negated

---
+++ script/official/c21364070.lua
@@ -0,0 +1,178 @@
+--魔妖仙獣 独眼群主
+--Mayosenju Hitot
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Pendulum Summon
+	Pendulum.AddProcedure(c)
+	--Change its scale to 11
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.sctg)
+	e1:SetOperation(s.scop)
+	c:RegisterEffect(e1)
+	--Return itself to the hand during the End Phase
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(function(e,tp) return Duel.IsTurnPlayer(tp) end)
+	e2:SetTarget(s.sthtg)
+	e2:SetOperation(s.sthop)
+	c:RegisterEffect(e2)
+	--Cannot be Special Summoned, except by Pendulum Summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e3:SetValue(aux.penlimit)
+	c:RegisterEffect(e3)
+	--Return 1 card the opponent controls to the hand
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_SUMMON_SUCCESS)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e5)
+	--Increase the ATK of "Yosenju" monsters by 500
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,3))
+	e6:SetCategory(CATEGORY_ATKCHANGE)
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e6:SetCode(EVENT_TO_HAND)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCondition(s.atkcond)
+	e6:SetOperation(s.atkop)
+	c:RegisterEffect(e6)
+	local e7=e6:Clone()
+	e7:SetCode(EVENT_TO_DECK)
+	e7:SetCondition(s.atkcond2)
+	c:RegisterEffect(e7)
+	--Return itself to the hand during the End Phase of the turn it is Special Summoned
+	local e8=Effect.CreateEffect(c)
+	e8:SetDescription(aux.Stringid(id,4))
+	e8:SetCategory(CATEGORY_TOHAND)
+	e8:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e8:SetRange(LOCATION_MZONE)
+	e8:SetCode(EVENT_PHASE+PHASE_END)
+	e8:SetCountLimit(1)
+	e8:SetCondition(function(e) return e:GetHandler():GetFlagEffect(id)~=0 end)
+	e8:SetTarget(s.rettg)
+	e8:SetOperation(s.retop)
+	c:RegisterEffect(e8)
+	aux.GlobalCheck(s,function()
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
+		ge1:SetLabel(id)
+		ge1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		ge1:SetOperation(aux.sumreg)
+		Duel.RegisterEffect(ge1,0)
+	end)
+end
+s.listed_series={SET_YOSENJU}
+function s.sctg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_PZONE) and chkc:IsControler(tp) and chkc:IsSetCard(SET_YOSENJU) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsSetCard,tp,LOCATION_PZONE,0,1,nil,SET_YOSENJU) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,Card.IsSetCard,tp,LOCATION_PZONE,0,1,1,nil,SET_YOSENJU)
+end
+function s.scop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Cannot Special Summon, except "Yosenju" monsters
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e0:SetDescription(aux.Stringid(id,4))
+	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e0:SetTargetRange(1,0)
+	e0:SetTarget(s.splimit)
+	e0:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e0,tp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		--Change Scales to 11
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LSCALE)
+		e1:SetValue(11)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_CHANGE_RSCALE)
+		tc:RegisterEffect(e2)
+	end
+end
+function s.splimit(e,c)
+	return not c:IsSetCard(SET_YOSENJU)
+end
+function s.sthtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.sthop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.SendtoHand(c,nil,REASON_EFFECT)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsAbleToHand() and chkc:IsController(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards(e)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+	end
+end
+function s.rtfilter(c,e,tp)
+	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==tp
+end
+function s.atkcond(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.rtfilter,1,nil,e,tp)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSetCard,SET_YOSENJU),tp,LOCATION_MZONE,0,nil)
+	if #g==0 then return end
+	for tc in g:Iter() do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(500)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+end
+function s.rtfilter2(c,tp)
+	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==tp and not c:IsLocation(LOCATION_EXTRA)
+end
+function s.atkcond2(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.rtfilter2,1,nil,tp) and not eg:IsExists(Card.IsLocation,1,nil,LOCATION_HAND)
+end
+function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoHand(c,nil,REASON_EFFECT)
+	end
+end

94108c85eb89fe8f73e55840b1beb28856d069c0
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-02-20 16:38:04 +0000

    Added a counter constant
    Added a constant for "Signal Counter" and updated relevant scripts. Also includes minor updates and re-scripting for "Signal Warrior" and "Ready! Set! Duel!".

---
+++ script/official/c31006879.lua
@@ -0,0 +1,72 @@
+--ライディング・デュエル！アクセラレーション！
+--Ready! Set! Duel!
+local s,id=GetID()
+function s.initial_effect(c)
+	--Search 1 "Synchron" monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--Place 1 Signal Counter on this card
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_COUNTER)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(function(_,tp) return Duel.IsTurnPlayer(tp) end)
+	e2:SetOperation(function(e) e:GetHandler():AddCounter(COUNTER_SIGNAL,1) end)
+	c:RegisterEffect(e2)
+	--Draw 2 then send 1 to the GY ("Speed Spell - Angel Baton")
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCost(s.drcost)
+	e3:SetTarget(s.drtg)
+	e3:SetOperation(s.drop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={SET_SYNCHRON}
+s.counter_place_list={COUNTER_SIGNAL}
+function s.thfilter(c,e,tp)
+	return c:IsSetCard(SET_SYNCHRON) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if #g>0 and not Duel.IsExistingMatchingCard(nil,tp,LOCATION_ONFIELD,0,1,e:GetHandler())
+		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
+function s.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToGraveAsCost()
+		and Duel.IsCanRemoveCounter(tp,1,0,COUNTER_SIGNAL,2,REASON_COST) end
+	Duel.RemoveCounter(tp,1,0,COUNTER_SIGNAL,2,REASON_COST)
+	Duel.SendtoGrave(c,REASON_COST)
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	if Duel.Draw(p,d,REASON_EFFECT)==2 then
+		Duel.ShuffleHand(p)
+		Duel.BreakEffect()
+		Duel.DiscardHand(p,nil,1,1,REASON_EFFECT)
+	end
+end
---
+++ script/official/c9634146.lua
@@ -0,0 +1,106 @@
+--シグナル・ウォリアー
+--Signal Warrior
+--Scripted by AlphaKretin
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Synchro Summon procedure
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	--Place Signal Counters
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_COUNTER)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetOperation(s.ctop)
+	c:RegisterEffect(e1)
+	--Cannot be destroyed by battle
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(function(e) return e:GetHandler():GetCounter(COUNTER_SIGNAL)>0 end)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--Cannot be destroyed by your opponent's card effects
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetValue(aux.indoval)
+	c:RegisterEffect(e3)
+	--Remove Signal Counters and apply 1 effect
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetCost(s.effcost)
+	e4:SetOperation(s.effop)
+	c:RegisterEffect(e4)
+end
+s.counter_place_list={COUNTER_SIGNAL}
+function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,0,LOCATION_FZONE,LOCATION_FZONE,nil)
+	if c:IsRelateToEffect(e) then g:AddCard(c) end
+	for tc in g:Iter() do
+		if tc:IsCanAddCounter(COUNTER_SIGNAL,1) then
+			tc:AddCounter(COUNTER_SIGNAL,1)
+		end
+	end
+end
+function s.effcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=Duel.IsCanRemoveCounter(tp,1,1,COUNTER_SIGNAL,4,REASON_COST)
+	local b2=Duel.IsCanRemoveCounter(tp,1,1,COUNTER_SIGNAL,7,REASON_COST)
+		and Duel.IsPlayerCanDraw(tp,1)
+	local b3=Duel.IsCanRemoveCounter(tp,1,1,COUNTER_SIGNAL,10,REASON_COST)
+		and Duel.IsExistingMatchingCard(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+	if chk==0 then return b1 or b2 or b3 end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,2)},
+		{b2,aux.Stringid(id,3)})
+	e:SetLabel(op)
+	local ct=4+(op-1)*3
+	Duel.RemoveCounter(tp,1,1,COUNTER_SIGNAL,ct,REASON_COST)
+	if op==1 then
+		e:SetCategory(CATEGORY_DAMAGE)
+		e:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		Duel.SetTargetPlayer(1-tp)
+		Duel.SetTargetParam(800)
+		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,800)
+	elseif op==2 then
+		e:SetCategory(CATEGORY_DRAW)
+		e:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		Duel.SetTargetPlayer(tp)
+		Duel.SetTargetParam(1)
+		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	elseif op==3 then
+		e:SetCategory(CATEGORY_DESTROY)
+		e:SetProperty(0)
+		local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,tp,0)
+	end
+end
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
+	local op=e:GetLabel()
+	if op==1 then
+		--Inflict 800 damage to your opponent
+		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+		Duel.Damage(p,d,REASON_EFFECT)
+	elseif op==2 then
+		--Draw 1 card
+		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+		Duel.Draw(p,d,REASON_EFFECT)
+	elseif op==3 then
+		--Destroy 1 card on the field
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if #g>0 then
+			Duel.HintSelection(g,true)
+			Duel.Destroy(g,REASON_EFFECT)
+		end
+	end
+end

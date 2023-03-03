de7fa35e7661f3f1c8a106c85bea3555a3433fca
Naim <naimsantos45@gmail.com>
2023-03-03 16:29:21 +0000

    Update "Number 44: Sky Pegasus"
    Cards like Stardust Dragon should be able to negate the activation of its effect

---
+++ script/official/c80764541.lua
@@ -0,0 +1,41 @@
+--No.44 白天馬スカイ・ペガサス
+--Number 44: Sky Pegasus
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Xyz Summon Procedure
+	Xyz.AddProcedure(c,nil,4,2)
+	--Destroy 1 face-up monster your opponent controls
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(aux.dxmcostgen(1,1,nil))
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+end
+s.xyz_number=44
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.CheckLPCost(1-tp,1000) and Duel.SelectYesNo(1-tp,aux.Stringid(id,1)) then
+		Duel.PayLPCost(1-tp,1000)
+		if Duel.IsChainDisablable(0) then
+			Duel.NegateEffect(0)
+		end
+	else
+		local tc=Duel.GetFirstTarget()
+		if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+			Duel.Destroy(tc,REASON_EFFECT)
+		end
+	end
+end

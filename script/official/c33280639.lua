211be7390ca5c29420c9d3c1e7c1ebe6bb8742ef
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-03-09 01:03:09 +0000

    "Prologue of the Destruction Swordsman" fix
    Was always checking if the player had an empty zone from the Extra Deck for the monster to be summoned, making the player unable to choose a "Buster Dragon" in the GY in some scenarios + shouldn't be able to use its GY effect if it has already successfully resolved earlier in the same turn. Also includes general script polishing and updates.

---
+++ script/official/c33280639.lua
@@ -0,0 +1,85 @@
+--破壊剣士の揺籃
+--Prologue of the Destruction Swordsman
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--Make "Destruction Sword" cards you control unanble to be destroyed by battle or card effects
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCost(aux.bfgcost)
+	e2:SetCondition(function(_,tp) return not Duel.HasFlagEffect(tp,id) end)
+	e2:SetOperation(s.immop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_DESTRUCTION_SWORD,SET_BUSTER_BLADER}
+s.listed_names={id,11790356}
+function s.cfilter(c)
+	return ((c:IsSetCard(SET_DESTRUCTION_SWORD) and not c:IsCode(id)) or (c:IsSetCard(SET_BUSTER_BLADER) and c:IsMonster()))
+		and c:IsAbleToGraveAsCost()
+end
+function s.rescon(sg,e,tp,mg)
+	return sg:IsExists(Card.IsSetCard,1,nil,SET_DESTRUCTION_SWORD)
+		and sg:IsExists(aux.AND(Card.IsSetCard,Card.IsMonster),1,nil,SET_BUSTER_BLADER)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_DECK,0,nil)
+	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) end
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_TOGRAVE,s.rescon)
+	Duel.SendtoGrave(sg,REASON_COST)
+end
+function s.spfilter(c,e,tp)
+	if not (c:IsCode(11790356) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)) then return false end
+	if c:IsLocation(LOCATION_EXTRA) then
+		return Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+	else
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA|LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA|LOCATION_GRAVE)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_EXTRA|LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
+	if sc and Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP) then
+		--Destroy it during the End Phase of the next turn
+		local turn_count=Duel.GetTurnCount()
+		aux.DelayedOperation(sc,PHASE_END,id,e,tp,
+			function(ag) Duel.Destroy(ag,REASON_EFFECT) end,
+			function() return Duel.GetTurnCount()==turn_count+1 end,
+			nil,2
+		)
+	end
+	Duel.SpecialSummonComplete()
+end
+function s.immop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.HasFlagEffect(tp,id) then return end
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
+	--Your "Destruction Sword" cards cannot be destroyed by battle or card effects
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetTargetRange(LOCATION_ONFIELD,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,SET_DESTRUCTION_SWORD))
+	e1:SetValue(1)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	Duel.RegisterEffect(e2,tp)
+end

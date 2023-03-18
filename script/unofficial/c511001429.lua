2fc9495c27018f0c059cd0bfa10bd12720048c5d
Larry126 <28985622+larry126@users.noreply.github.com>
2023-03-18 17:41:41 +0000

    Unofficial script fixes
    - NoC102 was missing the material count check for its 2nd effect
    - Crimson Fire (Anime) now correctly checks the ATK and damage

---
+++ script/unofficial/c511001429.lua
@@ -0,0 +1,170 @@
+--CNo.102 光堕天使ノーブル・デーモン (Anime)
+--Number C102: Archfiend Seraph (Anime)
+--Fixed by Larry126
+Duel.LoadScript("rankup_functions.lua")
+Duel.LoadCardScript("c67173574.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_LIGHT),5,4)
+	c:EnableReviveLimit()
+	--Rank Up Check
+	aux.EnableCheckRankUp(c,nil,nil,49678559)
+	--battle indestructable
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(aux.NOT(aux.TargetBoolFunction(Card.IsSetCard,0x48)))
+	c:RegisterEffect(e1)
+	--atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DISABLE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.tg)
+	e2:SetOperation(s.op)
+	e2:SetLabel(RESET_EVENT|RESETS_STANDARD)
+	--indes
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DAMAGE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e3:SetCondition(s.indescon)
+	e3:SetCost(s.indescost)
+	e3:SetTarget(s.indestg)
+	e3:SetOperation(s.indesop)
+	e3:SetLabel(RESET_EVENT|RESETS_STANDARD)
+	local e4=e3:Clone()
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetCondition(s.indescon2)
+	e4:SetLabel(RESET_EVENT|RESETS_STANDARD)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_RANKUP_EFFECT)
+	e5:SetLabelObject(e2)
+	c:RegisterEffect(e5,false,REGISTER_FLAG_DETACH_XMAT)
+	local e6=e5:Clone()
+	e6:SetLabelObject(e3)
+	c:RegisterEffect(e6,false,REGISTER_FLAG_DETACH_XMAT)
+	local e7=e5:Clone()
+	e7:SetLabelObject(e4)
+	c:RegisterEffect(e7,false,REGISTER_FLAG_DETACH_XMAT)
+end
+s.listed_series={0x48}
+s.xyz_number=102
+s.listed_names={49678559}
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.filter(c)
+	return c:IsFaceup() and (c:GetAttack()>0 or not c:IsDisabled())
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.HintSelection(g)
+	local tc=g:GetFirst()
+	if tc then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+		tc:RegisterEffect(e2)
+		if not tc:IsImmuneToEffect(e1) and not tc:IsImmuneToEffect(e2) then
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e3:SetValue(0)
+			e3:SetReset(RESET_EVENT|RESETS_STANDARD)
+			tc:RegisterEffect(e3)
+		end
+	end
+end
+function s.indescon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetHandler()
+	local bc=tc:GetBattleTarget()
+	if not bc then return false end
+	if tc:IsHasEffect(EFFECT_INDESTRUCTABLE_BATTLE) then
+		local tcind={tc:GetCardEffect(EFFECT_INDESTRUCTABLE_BATTLE)}
+		for _,te in ipairs(tcind) do
+			local f=te:GetValue()
+			if type(f)=='function' then
+				if f(te,bc) then return false end
+			else return false end
+		end
+	end
+	if bc==Duel.GetAttackTarget() and bc:IsDefensePos() then return false end
+	if bc:IsPosition(POS_FACEUP_DEFENSE) and bc==Duel.GetAttacker() then
+		if not bc:IsHasEffect(EFFECT_DEFENSE_ATTACK) then return false end
+		if bc:IsHasEffect(75372290) then
+			if tc:IsAttackPos() then
+				return bc:GetAttack()>0 and bc:GetAttack()>=tc:GetAttack()
+			else
+				return bc:GetAttack()>tc:GetDefense()
+			end
+		else
+			if tc:IsAttackPos() then
+				return bc:GetDefense()>0 and bc:GetDefense()>=tc:GetAttack()
+			else
+				return bc:GetDefense()>tc:GetDefense()
+			end
+		end
+	else
+		if tc:IsAttackPos() then
+			return bc:GetAttack()>0 and bc:GetAttack()>=tc:GetAttack()
+		else
+			return bc:GetAttack()>tc:GetDefense()
+		end
+	end
+end
+function s.indescon2(e,tp,eg,ep,ev,re,r,rp)
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
+	return ex and tg and tg:IsContains(e:GetHandler())
+end
+function s.indescost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local ct=c:GetOverlayCount()
+	if chk==0 then return ct>0 and c:CheckRemoveOverlayCard(tp,ct,REASON_COST) end
+	c:RemoveOverlayCard(tp,ct,ct,REASON_COST)
+end
+function s.indestg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local atk=e:GetHandler():GetAttack()
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(atk)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,atk)
+end
+function s.indesop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		if e:GetCode()==EVENT_PRE_DAMAGE_CALCULATE then
+			e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+			e1:SetValue(1)
+			e1:SetReset(RESET_PHASE|PHSE_DAMAGE)
+		else
+			e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+			e1:SetValue(function(e,te) return re==te end)
+			e1:SetReset(RESET_CHAIN)
+		end
+		c:RegisterEffect(e1)
+		local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+		Duel.Damage(p,c:GetAttack(),REASON_EFFECT)
+	end
+end
---
+++ script/unofficial/c511010530.lua
@@ -0,0 +1,63 @@
+--クリムゾン・ヘルフレア (Anime)
+--Crimson Fire (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--reflect
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c,ev)
+	return c:IsFaceup() and c:GetAttack()>ev
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	if rp==tp then return false end
+	local e1=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_DAMAGE)
+	local e2=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_RECOVER)
+	local rd=e1 and not e2
+	local rr=not e1 and e2
+	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_DAMAGE)
+	if ex and (cp==tp or cp==PLAYER_ALL) and not rd and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE) then
+		return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil,cv)
+	end
+	ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_RECOVER)
+	return ex and (cp==tp or cp==PLAYER_ALL) and rr and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE)
+		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil,cv)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_REFLECT_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetLabel(cid)
+	e1:SetValue(s.refcon)
+	e1:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(e1,tp)
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CHANGE_DAMAGE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(0,1)
+	e2:SetLabel(cid)
+	e2:SetValue(s.dammul)
+	e2:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(e2,tp)
+end
+function s.refcon(e,re,val,r,rp,rc)
+	local cc=Duel.GetCurrentChain()
+	if cc==0 or (r&REASON_EFFECT)==0 then return end
+	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
+	return cid==e:GetLabel()
+end
+function s.dammul(e,re,val,r,rp,rc)
+	local cc=Duel.GetCurrentChain()
+	if cc==0 or (r&REASON_EFFECT)==0 then return end
+	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
+	return cid==e:GetLabel() and val*2 or val
+end

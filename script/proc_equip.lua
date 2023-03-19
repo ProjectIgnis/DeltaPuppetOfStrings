725f55f3de707890750e1952e4f18c1bfdb3d041
Larry126 <28985622+larry126@users.noreply.github.com>
2023-03-19 21:24:22 +0000

    Equip procedure update
    Removed a workaround that was responsible for handling the Equip Spell still getting equipped even if the effect of the Equip Spell's activation was negated and replaced it with a proper implementation. This should also fix the bug that occurs when you target an Equip Spell with "Double Spell".

---
+++ script/proc_equip.lua
@@ -0,0 +1,67 @@
+--add procedure to equip spells equipping by rule
+function Auxiliary.AddEquipProcedure(c,p,f,eqlimit,cost,tg,op,con)
+	--Note: p==0 is check equip spell controler, p==1 for opponent's, PLAYER_ALL for both player's monsters
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(1068)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_CONTINUOUS_TARGET)
+	if con then
+		e1:SetCondition(con)
+	end
+	if cost~=nil then
+		e1:SetCost(cost)
+	end
+	e1:SetTarget(Auxiliary.EquipTarget(tg,p,f))
+	e1:SetOperation(Auxiliary.EquipOperation(op))
+	c:RegisterEffect(e1)
+	--Equip limit
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_EQUIP_LIMIT)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	if eqlimit~=nil then
+		e2:SetValue(eqlimit)
+	else
+		e2:SetValue(Auxiliary.EquipLimit(f))
+	end
+	c:RegisterEffect(e2)
+	return e1
+end
+function Auxiliary.EquipLimit(f)
+	return function(e,c)
+				return not f or f(c,e,e:GetHandlerPlayer())
+			end
+end
+function Auxiliary.EquipFilter(c,p,f,e,tp)
+	return (p==PLAYER_ALL or c:IsControler(p)) and c:IsFaceup() and (not f or f(c,e,tp))
+end
+function Auxiliary.EquipTarget(tg,p,f)
+	return	function(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+				local player=nil
+				if p==0 then
+					player=tp
+				elseif p==1 then
+					player=1-tp
+				elseif p==PLAYER_ALL or p==nil then
+					player=PLAYER_ALL
+				end
+				if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and Auxiliary.EquipFilter(chkc,player,f,e,tp) end
+				if chk==0 then return player~=nil and Duel.IsExistingTarget(Auxiliary.EquipFilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,player,f,e,tp) end
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+				local g=Duel.SelectTarget(tp,Auxiliary.EquipFilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,player,f,e,tp)
+				if tg then tg(e,tp,eg,ep,ev,re,r,rp,g:GetFirst()) end
+				Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+			end
+end
+function Auxiliary.EquipOperation(op)
+	return	function(e,tp,eg,ep,ev,re,r,rp)
+				local tc=Duel.GetFirstTarget()
+				if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+					Duel.Equip(tp,e:GetHandler(),tc)
+				end
+				if op then op(e,tp,eg,ep,ev,re,r,rp) end
+			end
+end

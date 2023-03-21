054d9d5e8f29e5f2bbb353782c4bac7aee1b8bb9
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-03-21 12:11:11 +0000

    "Branded in White" fix
    Should be considered to mention "Fallen of Albaz" in its text.

---
+++ script/official/c34995106.lua
@@ -0,0 +1,74 @@
+--白の烙印
+--Branded in White
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion Summon
+	local e1=Fusion.CreateSummonEff({handler=c,extrafil=s.fextra,extraop=s.extraop,extratg=s.extratg})
+	e1:SetCountLimit(1,id)
+	c:RegisterEffect(e1)
+	--Set itself during the End Phase
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetOperation(s.regop)
+	c:RegisterEffect(e2)
+end
+s.listed_names={CARD_ALBAZ}
+function s.fcheck(tp,sg,fc)
+	if not sg:IsExists(Card.IsRace,1,nil,RACE_DRAGON) then
+		return false
+	end
+	if sg:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE) then
+		return sg:IsExists(Card.IsCode,1,nil,CARD_ALBAZ)
+	end
+	return true
+end
+function s.fextra(e,tp,mg)
+	local eg=Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
+	if #eg>0 and (mg+eg):IsExists(Card.IsCode,1,nil,CARD_ALBAZ) then
+		if #eg>0 then
+			return eg,s.fcheck
+		end
+	end
+	return nil,s.fcheck
+end
+function s.extraop(e,tc,tp,sg)
+	local rg=sg:Filter(Card.IsLocation,nil,LOCATION_GRAVE)
+	if #rg>0 then
+		Duel.Remove(rg,POS_FACEUP,REASON_EFFECT|REASON_MATERIAL|REASON_FUSION)
+		sg:Sub(rg)
+	end
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_GRAVE)
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsReason(REASON_COST) and re and re:IsActivated() and re:GetHandler():IsCode(CARD_ALBAZ) then
+		--Set itself from the GY
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(aux.Stringid(id,1))
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetRange(LOCATION_GRAVE)
+		e1:SetCountLimit(1,{id,1})
+		e1:SetTarget(s.settg)
+		e1:SetOperation(s.setop)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsSSetable() end
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,c,1,tp,0)
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsSSetable() then
+		Duel.SSet(tp,c)
+	end
+end

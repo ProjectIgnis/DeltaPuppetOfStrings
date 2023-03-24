46287f35820d0e347e422f1841098ffa5b5c78ba
Naim <naimsantos45@gmail.com>
2023-03-24 09:05:34 +0000

    "Psychic Blade" fix
    Fixed a bug that would cause it to be sent to the GY in the initial activation

---
+++ script/official/c75539614.lua
@@ -0,0 +1,71 @@
+--サイコ・ブレイド
+--Psychic Blade
+local s,id=GetID()
+function s.initial_effect(c)
+	--Pay LP to equip this card
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--Equipped monster gain ATK/DEF equal to the LP paid
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(s.val)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e3)
+	--Equip limit
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_EQUIP_LIMIT)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetValue(1)
+	c:RegisterEffect(e4)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	for _,te in ipairs({Duel.GetPlayerEffect(tp,EFFECT_LPCOST_CHANGE)}) do
+		local val=te:GetValue()
+		if val(te,e,tp,100)~=100 then return false end
+	end
+	return true
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,100) end
+	local lp=Duel.GetLP(tp)
+	local m=math.floor(math.min(lp,2000)/100)
+	local t={}
+	for i=1,m do
+		t[i]=i*100
+	end
+	local ac=Duel.AnnounceNumber(tp,table.unpack(t))
+	Duel.PayLPCost(tp,ac)
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1,ac)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,tp,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Equip(tp,c,tc)
+	end
+end
+function s.val(e,c)
+	local ct=e:GetHandler():GetFlagEffectLabel(id)
+	if not ct then return 0 end
+	return ct
+end

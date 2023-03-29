c6f158478beed41c22a9456a1e1a6041332193c8
Edoardo Lolletti <edoardo762@gmail.com>
2023-03-29 09:40:25 +0000

    Improve "There Can Be Only One" handling (#704)
    * Improve there can be only one checking:
    Fixed an issue where the global state internal to the card would have been leaked when the card got destroyed, also cleaned up the handling of the checking itself, now the card will keep track of the highest field id, and automatically send to the grave cards with a greater id (in case of returning to field and control changing, that are the only possible actions that would make a card touch the field), handling of face down monsters is undefined behaviour as now
    
    * Fix handling of greatest field id changing without going first to a zero monsters step:
    Properly handle situations where, for example, a player swapped his last monster with the opponent's, in which case the swapped monster would've been sent to the grave instead of being registered as a valid card.

---
+++ script/official/c24207889.lua
@@ -0,0 +1,100 @@
+--センサー万別
+--There Can Be Only One
+--Scripted by edo9300
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_STANDBY_PHASE,TIMING_STANDBY_PHASE+TIMINGS_CHECK_MONSTER)
+	c:RegisterEffect(e1)
+	--adjust
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(id)
+	e2:SetRange(LOCATION_SZONE)
+	c:RegisterEffect(e2)
+	--cannot summon,spsummon,flipsummon
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCode(EFFECT_FORCE_SPSUMMON_POSITION)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetTargetRange(1,1)
+	e4:SetTarget(s.sumlimit)
+	e4:SetValue(POS_FACEDOWN)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetCode(EFFECT_CANNOT_SUMMON)
+	c:RegisterEffect(e5)
+	local e6=e4:Clone()
+	e6:SetCode(EFFECT_CANNOT_FLIP_SUMMON)
+	c:RegisterEffect(e6)
+	aux.GlobalCheck(s,function()
+		s.lastFieldId={}
+		s.lastFieldId[0]=nil
+		s.lastFieldId[1]=nil
+		local ge=Effect.GlobalEffect()
+		ge:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		ge:SetCode(EVENT_ADJUST)
+		ge:SetOperation(s.adjustop)
+		Duel.RegisterEffect(ge,0)
+	end)
+end
+function s.sumlimit(e,c,sump,sumtype,sumpos,targetp)
+	local tp=sump
+	if targetp then tp=targetp end
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,c:GetRace()),tp,LOCATION_MZONE,0,1,c)
+end
+function s.fidfilter(c,fid)
+	return c:GetFieldID()>fid
+end
+local faceupfil=aux.FilterFaceupFunction(Card.IsHasEffect,id)
+function s.adjustop(e,tp,eg,ep,ev,re,r,rp)
+	local phase=Duel.GetCurrentPhase()
+	if (phase==PHASE_DAMAGE and not Duel.IsDamageCalculated()) or phase==PHASE_DAMAGE_CAL then return end
+	if not Duel.IsExistingMatchingCard(faceupfil,0,LOCATION_SZONE,LOCATION_SZONE,1,nil) then
+		s.lastFieldId[0]=nil
+		s.lastFieldId[1]=nil
+		return
+	end
+	local sg=Group.CreateGroup()
+	for p=0,1 do
+		local g=Duel.GetMatchingGroup(Card.IsFaceup,p,LOCATION_MZONE,0,nil)
+		if #g==0 then
+			s.lastFieldId[p]=nil
+		else
+			local race=1
+			local update_fid=false
+			while (RACE_ALL&race)~=0 do
+				local rg=g:Filter(Card.IsRace,nil,race)
+				if s.lastFieldId[p] then
+					local forced
+					forced,rg=rg:Split(s.fidfilter,nil,s.lastFieldId[p])
+					if #rg==0 then
+						rg=forced
+						update_fid=true
+					else
+						sg:Merge(forced)
+					end
+				end
+				local rc=#rg
+				if rc>1 then
+					Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TOGRAVE)
+					sg:Merge(rg:Select(p,rc-1,rc-1,nil))
+				end
+				race=race<<1
+			end
+			if update_fid or not s.lastFieldId[p] then
+				local maxg,maxid=g:Sub(sg):GetMaxGroup(Card.GetFieldID)
+				s.lastFieldId[p]=maxid
+			end
+		end
+	end
+	if #sg>0 then
+		Duel.SendtoGrave(sg,REASON_RULE)
+		Duel.Readjust()
+	end
+end

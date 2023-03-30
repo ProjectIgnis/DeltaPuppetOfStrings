df3ddf8c6860763f9c435e7ebfea80343b93d2fc
Naim <naimsantos45@gmail.com>
2023-03-30 16:43:28 +0000

    Rush updates and fixes
    -Renamed constant (CARD_TOUGHROID) to match the official card name (CARD_PRINTING_PRESSER)
    -Fixed incorrect materials used by "Newsflash the Journalistic Juggernaut"
    -General script clean-up for all cards that mention "Printing Presser"

---
+++ script/rush/c160007037.lua
@@ -0,0 +1,52 @@
+--報道艦轟鎧號 疾風迅雷
+--Newsflash the Journalistic Juggernaut
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Fusion materials
+	Fusion.AddProcMix(c,true,true,CARD_PRINTING_PRESSER,160007007)
+	--Make monsters lose ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(1-tp,7) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
+	--Effect
+	Duel.ConfirmDecktop(1-tp,7)
+	local g=Duel.GetDecktopGroup(1-tp,7)
+	local sg=g:Filter(Card.IsMonster,nil)
+	local val=0
+	if #sg>0 then
+		val=#sg*(-500)
+	end
+	Duel.SortDecktop(1-tp,1-tp,7)
+	local g2=Duel.GetMatchingGroup(aux.FilterMaximumSideFunctionEx(Card.IsFaceup),tp,0,LOCATION_MZONE,nil)
+	if #g2==0 then return end
+	if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		for tc in g2:Iter() do
+			--Decrease ATK
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(val)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+			tc:RegisterEffectRush(e1)
+		end
+	end
+end

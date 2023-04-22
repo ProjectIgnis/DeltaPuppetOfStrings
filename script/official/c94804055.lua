8700377ff2ef2e906064f399497a33f46e8bbdfb
Naim <naimsantos45@gmail.com>
2023-04-22 14:27:42 +0000

    "Firewall" fix
    -Fixed a bug where the player would be unable to use the effect if the activation of the card occured when an attack was declared
    -Fixed a bug where monsters that cannot be targeted could not have their attacks negated

---
+++ script/official/c94804055.lua
@@ -0,0 +1,54 @@
+--ファイヤー・ウォール
+--Firewall
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Negate a direct attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetCondition(s.atkcon)
+	e2:SetCost(s.atkcost)
+	e2:SetTarget(s.atktg)
+	e2:SetOperation(function() Duel.NegateAttack() end)
+	c:RegisterEffect(e2)
+	--Maintenance cost
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(function(e,tp) return Duel.IsTurnPlayer(tp) end)
+	e3:SetOperation(s.mtop)
+	c:RegisterEffect(e3)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker():IsControler(1-tp) and Duel.GetAttackTarget()==nil
+end
+function s.cfilter(c)
+	return c:IsRace(RACE_PYRO) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
+end
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetFlagEffect(id)==0 end
+	e:GetHandler():RegisterFlagEffect(id,RESET_CHAIN,0,1)
+end
+function s.mtop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.CheckLPCost(tp,500) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.PayLPCost(tp,500)
+	else
+		Duel.Destroy(e:GetHandler(),REASON_COST)
+	end
+end

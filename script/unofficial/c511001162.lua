c689fd30a067872e8904c434a9c360969abb5945
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-04-22 18:01:36 +0000

    Fix "Trick Buster"
    Should not be able to activate if an opponent attacks directly while you control a monster.

---
+++ script/unofficial/c511001162.lua
@@ -0,0 +1,37 @@
+--トリック・バスター (Anime)
+--Trick Buster (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	return ep==1-tp and Duel.GetAttackTarget()==nil and #g==0
+end
+function s.desfilter(c)
+	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsDestructable()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	if #g~=0 then
+		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,#g*300)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_MZONE,nil)
+	local ct=Duel.Destroy(g,REASON_EFFECT)
+	if ct~=0 then
+		Duel.BreakEffect()
+		Duel.Damage(1-tp,ct*300,REASON_EFFECT)
+	end
+end

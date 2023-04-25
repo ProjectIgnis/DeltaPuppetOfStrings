112bd2b5f3a3230cdd695df98d02a2c92d304a62
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-04-25 03:13:00 +0000

    Update "Yubel (Anime)"
    Damage Reflect should work regardless of position/change SS to optional trigger inline with text.

---
+++ script/unofficial/c511001391.lua
@@ -0,0 +1,75 @@
+--ユベル (Anime)
+--Yubel (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Cannot be destroyed by battle
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--Your opponent takes all battle damage from battles involving this monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_REFLECT_BATTLE_DAMAGE)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--Tribute 1 monster or destroy this card
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(78371393,1))
+	e3:SetCategory(CATEGORY_RELEASE+CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.descon)
+	e3:SetTarget(s.destg)
+	e3:SetOperation(s.desop)
+	c:RegisterEffect(e3)
+	--Special Summon "Yubel - Terror Incarnate" if destroyed
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(78371393,3))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_DESTROYED)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e4:SetCondition(s.spcon)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
+	e4:SetLabelObject(e3)
+	c:RegisterEffect(e4)
+end
+s.listed_names={4779091}
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not e:GetHandler():IsLocation(LOCATION_DECK) end
+	if not Duel.CheckReleaseGroup(tp,nil,1,nil) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
+	end
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+	if Duel.CheckReleaseGroup(tp,Card.IsReleasableByEffect,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(78371393,2)) then
+		local g=Duel.SelectReleaseGroup(tp,Card.IsReleasableByEffect,1,1,nil)
+		Duel.Release(g,REASON_EFFECT)
+	else Duel.Destroy(c,REASON_EFFECT) end
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return re~=e:GetLabelObject()
+end
+function s.filter(c,e,tp)
+	return c:IsCode(4779091) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
+	if tc then Duel.SpecialSummon(tc,0,tp,tp,true,true,POS_FACEUP) end
+end

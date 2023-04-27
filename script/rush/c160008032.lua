3590eaf2fe9c1edb39c2a15bf036bf6b7dbf1d92
Naim <naimsantos45@gmail.com>
2023-04-27 13:41:25 +0000

    Fix "Master of Sevens Road"
    Fixed a bug where the ATK change effect would apply even if it was not face-up

---
+++ script/rush/c160008032.lua
@@ -0,0 +1,32 @@
+--マスター・オブ・セブンスロード
+--Master of Sevens Road
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Fusion Summon Procedure
+	Fusion.AddProcMix(c,true,true,CARD_SEVENS_ROAD_MAGICIAN,160401001)
+	--Gains ATK per each Spellcaster monster with different name
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.atkval)
+	c:RegisterEffect(e1)
+	--Inflict piercing battle damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_PIERCE)
+	e2:SetCondition(s.pcon)
+	c:RegisterEffect(e2)
+end
+function s.cfilter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_SPELLCASTER)
+end
+function s.atkval(e,c)
+	local g=Duel.GetMatchingGroup(s.cfilter,e:GetHandler():GetControler(),LOCATION_GRAVE,0,nil)
+	return g:GetClassCount(Card.GetCode)*300
+end
+function s.pcon(e)
+	return Duel.GetMatchingGroupCount(s.cfilter,e:GetHandler():GetControler(),LOCATION_GRAVE,0,nil)>=7
+end

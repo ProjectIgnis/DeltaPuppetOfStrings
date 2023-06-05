da35ed06fdf054d45fb6d5b8645a7a600902334e
Naim <naimsantos45@gmail.com>
2023-06-05 21:24:35 +0000

    "Labrynth Cooclock" fix
    Fixed a bug where the second effect would not be negated by "Necrovalley"

---
+++ script/official/c2511.lua
@@ -0,0 +1,76 @@
+-- 白銀の城の狂時計
+-- Labrynth Cooclock
+-- Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Allow Trap activation in the same turn it's set
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.accost)
+	e1:SetOperation(s.acop)
+	c:RegisterEffect(e1)
+	-- Add to hand or Special Summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.thcon)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+s.listed_names={id}
+s.listed_series={SET_LABRYNTH}
+function s.accost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsDiscardable() end
+	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
+end
+function s.acop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCountLimit(1,{id,2})
+	e1:SetTargetRange(LOCATION_SZONE,0)
+	e1:SetCondition(s.accon)
+	e1:SetTarget(function(e,c) return c:IsNormalTrap() end)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.accon(e)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,SET_LABRYNTH),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	if not (rp==tp and r&REASON_COST==REASON_COST and re and re:IsActivated()) then return false end
+	local rc=re:GetHandler()
+	return eg:IsExists(Card.IsPreviousLocation,1,e:GetHandler(),LOCATION_HAND)
+		and ((re:IsTrapEffect() and re:IsHasType(EFFECT_TYPE_ACTIVATE))
+		or (rc:IsSetCard(SET_LABRYNTH) and not rc:IsCode(id)))
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToHand() or (Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)) end
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,c,1,tp,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,c,1,tp,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	aux.ToHandOrElse(c,tp,
+		function(sc) return sc:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end,
+		function(sc) Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP) end,
+		aux.Stringid(id,3)
+	)
+end

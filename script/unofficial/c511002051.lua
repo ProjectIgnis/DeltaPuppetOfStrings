4f0400e642b637cbb51dc1fbedd087e3d6659e01
Larry126 <28985622+larry126@users.noreply.github.com>
2023-06-14 07:34:34 +0000

    Unofficial script fixes
    - Magical Trick Mirror (Anime): missing card target flag
    - Anime Ra: missing delay flag
    - Manga Ra: missing function
    And other minor cleanups

---
+++ script/unofficial/c511002051.lua
@@ -0,0 +1,35 @@
+--攻撃誘導アーマー (Anime)
+--Attack Guidance Armor (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetTarget(s.atktg)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	return c~=Duel.GetAttackTarget() and c~=Duel.GetAttacker()
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTACKTARGET)
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil):GetFirst()
+	if tc then
+		Duel.HintSelection(g)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetRange(LOCATION_SZONE)
+		e1:SetCode(EFFECT_SELF_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetReset(RESET_PHASE|PHASE_DAMAGE)
+		e1:SetTargetRange(1,1)
+		Duel.RegisterEffect(e1,tp)
+		Duel.ChangeAttackTarget(tc,true)
+	end
+end

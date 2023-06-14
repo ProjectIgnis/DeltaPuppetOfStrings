f2514061281313d9862fd50d90627a056e22d188
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-06-14 23:56:17 +0000

    Fix "Advanced Heraldry Art (Anime)"
    Moved location check to filter to process MR in a duel.

---
+++ script/unofficial/c511002746.lua
@@ -0,0 +1,31 @@
+--高等紋章術 (Anime)
+--Advanced Heraldry Art (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Xyz Summon using "Heraldic Beast" monsters in your GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={SET_HERALDIC_BEAST}
+function s.xyzfilter(c,tp,mg)
+	return c:IsXyzSummonable(nil,mg) and Duel.GetLocationCountFromEx(tp,tp,mg,c)>0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local mg=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_GRAVE,0,nil,SET_HERALDIC_BEAST)
+	if chk==0 then return #mg>1 and Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_EXTRA,0,1,nil,tp,mg) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local mg=Duel.GetMatchingGroup(Card.IsSetCard,tp,LOCATION_GRAVE,0,nil,SET_HERALDIC_BEAST)
+	local xyzg=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,tp,mg)
+	if #xyzg>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local xyz=xyzg:Select(tp,1,1,nil):GetFirst()
+		Duel.XyzSummon(tp,xyz,nil,mg,99,99)
+	end
+end

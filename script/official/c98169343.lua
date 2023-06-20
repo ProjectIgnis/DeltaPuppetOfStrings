7c24959a009193ef0ef037dbc6dd1d01157e212f
Naim <naimsantos45@gmail.com>
2023-06-20 14:10:19 +0000

    Fix "Trickstar Corobane"
    Fixed a bug where it could be activated even when your "Trickstar" monster was face-down

---
+++ script/official/c98169343.lua
@@ -0,0 +1,63 @@
+--トリックスター・キャロベイン
+--Trickstar Corobane
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon itself from the hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Increase the ATK of a "Trickstar" monster that battles an opponent's monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetHintTiming(TIMING_DAMAGE_STEP)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.atkcon)
+	e2:SetCost(s.atkcost)
+	e2:SetOperation(s.atkop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_TRICKSTAR}
+function s.cfilter(c)
+	return c:IsFacedown() or not c:IsSetCard(SET_TRICKSTAR)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local ph=Duel.GetCurrentPhase()
+	if ph~=PHASE_DAMAGE or Duel.IsDamageCalculated() then return false end
+	local tc,oc=Duel.GetBattleMonster(tp)
+	return tc and oc and tc:IsSetCard(SET_TRICKSTAR) and tc:IsFaceup()
+end
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc,oc=Duel.GetBattleMonster(tp)
+	if tc:IsFaceup() and tc:IsControler(tp) then
+		tc:UpdateAttack(tc:GetBaseAttack(),RESET_PHASE|PHASE_END,e:GetHandler())
+	end
+end

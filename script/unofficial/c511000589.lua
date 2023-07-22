e04b63e1385d592cf02163ca1f6eea8c006a67c2
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-07-22 01:38:41 +0000

    Update "Prediction Princess Coinorma (Anime)"
    The monster Set by effect should be Special Summoned in face-down Defense Position (i.e. in the same line as effects like Toadally Awesome and Goddess Verdande's Guidance.)

---
+++ script/unofficial/c511000589.lua
@@ -0,0 +1,33 @@
+--占術姫コインノーマ (Anime)
+--Prediction Princess Coinorma (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon 1 Level 4 or lower Flip monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.filter(c,e,tp)
+	return c:IsLevelBelow(4) and c:IsType(TYPE_FLIP) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.filter),tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,nil,e,tp)
+    	if #g>0 then
+        	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        	local sg=g:Select(tp,1,1,nil)
+        	Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEDOWN_DEFENSE)
+        	Duel.ConfirmCards(1-tp,sg)
+	end
+end
+

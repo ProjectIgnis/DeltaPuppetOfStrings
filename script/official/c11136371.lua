f5518ec3db0a69d7517fa3ff83fdb81690dfddf3
Naim <naimsantos45@gmail.com>
2023-07-31 20:37:01 +0000

    "Counter Gem" fix
    Fixed a bug where it would be possible to activate it even if a card couldn't be sent to the GY as cost

---
+++ script/official/c11136371.lua
@@ -0,0 +1,62 @@
+--カウンター・ジェム
+--Counter Gem
+local s,id=GetID()
+function s.initial_effect(c)
+	--Place as many "Crystal Beast" monsters as possible from your GY face-up in your S/Trap Zone
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.plcost)
+	e1:SetTarget(s.pltg)
+	e1:SetOperation(s.plop)
+	c:RegisterEffect(e1)
+end
+s.listed_series={SET_CRYSTAL_BEAST}
+function s.plcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_STZONE,0,e:GetHandler())
+	if chk==0 then return #g>0 and #g=g:FilterCount(Card.IsAbleToGraveAsCost,nil) end
+	g:Filter(Card.IsAbleToGraveAsCost,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
+function s.plfilter(c)
+	return c:IsSetCard(SET_CRYSTAL_BEAST) and not c:IsForbidden()
+end
+function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.plfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,nil,1,tp,LOCATION_GRAVE)
+end
+function s.plop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_SZONE)
+	if ft<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectMatchingCard(tp,s.plfilter,tp,LOCATION_GRAVE,0,ft,ft,nil)
+	if #g>0 then
+		for tc in g:Iter() do
+			Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetCode(EFFECT_CHANGE_TYPE)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e1:SetReset(RESET_EVENT|(RESETS_STANDARD&~RESET_TURN_SET))
+			e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+			tc:RegisterEffect(e1)
+		end
+		Duel.RaiseEvent(g,EVENT_CUSTOM+47408488,e,0,tp,0,0)--Event used by Crystal Tree
+	end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.descon)
+	e1:SetOperation(s.desop)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,SET_CRYSTAL_BEAST),tp,LOCATION_ONFIELD,0,1,nil)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsSetCard,SET_CRYSTAL_BEAST),tp,LOCATION_ONFIELD,0,nil)
+	Duel.Destroy(g,REASON_EFFECT)
+end

759a9472f72ae9c2b42bb92809b735a5a28a1496
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-08-01 21:26:50 +0000

    "Crystal Beast Rainbow Dragon" fix
    General fixes and polishing.

---
+++ script/official/c36795102.lua
@@ -0,0 +1,100 @@
+--究極宝玉獣 レインボー・ドラゴン
+--Crystal Beast Rainbow Dragon
+--Logical Nonsense
+local s,id=GetID()
+function s.initial_effect(c)
+	--Place itself in the Spell & Trap Zone as a Continuous Spell
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_TO_GRAVE_REDIRECT_CB)
+	e1:SetCondition(s.repcon)
+	e1:SetOperation(s.repop)
+	c:RegisterEffect(e1)
+	--Special Summon this card from your hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.selfspcon)
+	e2:SetTarget(s.selfsptg)
+	e2:SetOperation(s.selfspop)
+	c:RegisterEffect(e2)
+	--Special Summon 1 Level 4 or lower "Crystal Beast" monster from your Deck and search 1 "Ultimate Crystal" monster
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(function(e) return e:GetHandler():IsContinuousSpell() end)
+	e3:SetCost(aux.bfgcost)
+	e3:SetTarget(s.spthtg)
+	e3:SetOperation(s.spthop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={SET_CRYSTAL_BEAST,SET_ULTIMATE_CRYSTAL}
+function s.repcon(e)
+	local c=e:GetHandler()
+	return c:IsFaceup() and c:IsLocation(LOCATION_MZONE) and c:IsReason(REASON_DESTROY)
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Treated as a Continuous Spell
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EFFECT_CHANGE_TYPE)
+	e1:SetValue(TYPE_SPELL|TYPE_CONTINUOUS)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD&~RESET_TURN_SET)
+	c:RegisterEffect(e1)
+	Duel.RaiseEvent(c,EVENT_CUSTOM+47408488,e,0,tp,0,0)
+end
+function s.selfspcon(e,tp,eg,ep,ev,re,r,rp)
+	local dc=Duel.GetAttackTarget()
+	return Duel.GetAttacker():IsSetCard(SET_CRYSTAL_BEAST) or (dc and dc:IsFaceup() and dc:IsSetCard(SET_CRYSTAL_BEAST))
+end
+function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.spfilter(c,e,tp)
+	return c:IsLevelBelow(4) and c:IsSetCard(SET_CRYSTAL_BEAST) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,c)
+end
+function s.thfilter(c)
+	return c:IsMonster() and c:IsSetCard(SET_ULTIMATE_CRYSTAL) and c:IsAbleToHand()
+end
+function s.spthtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.spthop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+	if sc and Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP) then
+		--Negate its effects
+		sc:NegateEffects(e:GetHandler())
+	end
+	if Duel.SpecialSummonComplete()==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local hg=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #hg>0 then
+		Duel.SendtoHand(hg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,hg)
+	end
+end

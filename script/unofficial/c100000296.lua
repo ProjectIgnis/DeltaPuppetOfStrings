93f717822a8173b4588c66fec2faaea4654c60ca
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2023-08-09 15:59:05 +0000

    Update c100000296.lua
    Should not be activatable in the controller's Battle Phase/ minor script clean-up

---
+++ script/unofficial/c100000296.lua
@@ -0,0 +1,35 @@
+--ハーピィの羽根吹雪 (Anime)
+--Harpie's Feather Storm (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={SET_HARPIE}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetTurnPlayer()==e:GetHandlerPlayer() then return false end
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,SET_HARPIE),tp,LOCATION_MZONE,0,1,nil)
+		and re:IsMonsterEffect() and Duel.IsBattlePhase() and tp~=ep
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateActivation(ev)
+	if re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+	Duel.SkipPhase(Duel.GetTurnPlayer(),PHASE_BATTLE,RESET_PHASE|PHASE_BATTLE,1)
+end

ed59fd6a4a42a6b1a5d76dfa899cb12512c185dd
Larry126 <28985622+larry126@users.noreply.github.com>
2023-08-15 14:08:42 +0000

    Update "Super Polymerization (Anime)" script

---
+++ script/unofficial/c511003010.lua
@@ -0,0 +1,39 @@
+--超融合 (Anime)
+--Super Polymerization (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff{handler=c,matfilter=Fusion.OnFieldMat,extrafil=s.fextra,extratg=s.extratg,extraop=s.extraop}
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e1:SetCost(s.cost)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsAbleToGraveAsCost,1,1,REASON_COST)
+end
+function s.check(tp,sg,fc)
+	return sg:IsExists(Card.IsControler,1,nil,tp)
+end
+function s.filter(c,p)
+	return c:IsFaceup() or c:IsControler(p)
+end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(s.filter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil,tp),s.check
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+		Duel.SetChainLimit(aux.FALSE)
+	end
+end
+function s.extraop(e,tc,tp,mat)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetOperation(s.sumsuc)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD&~RESET_TOFIELD)
+	tc:RegisterEffect(e1,true)
+end
+function s.sumsuc(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SetChainLimitTillChainEnd(aux.FALSE)
+end

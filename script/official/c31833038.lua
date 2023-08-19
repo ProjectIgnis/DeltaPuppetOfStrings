054da726afd751822b3deaa1cff0fc5606f4d311
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-08-19 10:42:07 +0000

    "Borreload Dragon" update
    They fixed that silly temp banishment ruling (OCG db Q&A # 13264).

---
+++ script/official/c31833038.lua
@@ -0,0 +1,94 @@
+--ヴァレルロード・ドラゴン
+--Borreload Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Link Summon procedure
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),3)
+	--Cannot be targeted by monster effects
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(function(e,re,rp) return re:IsMonsterEffect() end)
+	c:RegisterEffect(e1)
+	--Target monster loses 500 ATK/DEF
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(TIMING_DAMAGE_STEP+TIMING_END_PHASE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(function() return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated() end)
+	e2:SetTarget(s.atktg)
+	e2:SetOperation(s.atkop)
+	c:RegisterEffect(e2)
+	--Take control of an opponent's monster this card attacks
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_CONTROL)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BATTLE_START)
+	e3:SetCondition(s.ctcon)
+	e3:SetTarget(s.cttg)
+	e3:SetOperation(s.ctop)
+	c:RegisterEffect(e3)
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetChainLimit(function(_e,_ep,_tp) return _tp==_ep end)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		--It loses 500 ATK/DEF
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		e1:SetValue(-500)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_UPDATE_DEFENSE)
+		tc:RegisterEffect(e2)
+	end
+end
+function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetAttackTarget()
+	local zone=c:GetLinkedZone()&ZONES_MMZ
+	return Duel.GetAttacker()==c and tc and tc:IsControler(1-tp) and tc:IsControlerCanBeChanged(false,zone)
+end
+function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local tc=Duel.GetAttackTarget()
+	Duel.SetTargetCard(tc)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,tc,1,tp,0)
+end
+function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not (c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsControler(1-tp)) then return end
+	local zone=c:GetLinkedZone()&ZONES_MMZ
+	if Duel.GetControl(tc,tp,0,0,zone) then
+		local turn_ct=Duel.GetTurnCount()
+		--Send it to the GY during the End Phase of the next turn
+		aux.DelayedOperation(tc,PHASE_END,id,e,tp,
+			function(ag)
+				Duel.SendtoGrave(ag,REASON_EFFECT)
+			end,
+			function()
+				return Duel.GetTurnCount()==turn_ct+1
+			end,
+			nil,2
+		)
+	end
+end

8a600faeecc15f86f04ae69520d848d444a42389
Naim <naimsantos45@gmail.com>
2023-08-31 19:10:12 +0000

    Update some card constants
    Use the official TCG name in the constants for the following cards: Rikka Konkon, Vernusylph Corolla and Revealer of the Ice Barrier

---
+++ script/official/c14108995.lua
@@ -0,0 +1,46 @@
+-- 春化精の花冠
+-- Vernusylph Corolla
+-- Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	-- EARTH monsters become "Vernusylph" monsters
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_ADD_SETCODE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_EARTH))
+	e2:SetValue(0x183)
+	c:RegisterEffect(e2)
+	-- "Vernusylph" cost replacement
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(CARD_VERNUSYLPH_COROLLA)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_HAND,0)
+	e3:SetCountLimit(1)
+	e3:SetValue(s.repval)
+	e3:SetOperation(s.repop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={SET_VERNUSYLPH}
+function s.repval(base,e,tp,eg,ep,ev,re,r,rp,chk,extracon)
+	local c=e:GetHandler()
+	return c:IsMonster() and c:IsSetCard(SET_VERNUSYLPH) and c:IsDiscardable()
+end
+function s.repop(base,e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	local c=base:GetHandler()
+	if #{Duel.GetPlayerEffect(tp,CARD_VERNUSYLPH_COROLLA)}>1 then
+		Duel.HintSelection(c,true)
+	end
+	c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END,EFFECT_FLAG_CLIENT_HINT,1,nil,aux.Stringid(id,1))
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
---
+++ script/official/c76869711.lua
@@ -0,0 +1,64 @@
+-- 六花来々
+-- Rikka Konkon
+-- Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	-- Set 1 "Rikka" Spell/Trap from the Deck
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.setcon)
+	e2:SetTarget(s.settg)
+	e2:SetOperation(s.setop)
+	c:RegisterEffect(e2)
+	-- Replace 1 Plant tribute
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(CARD_RIKKA_KONKON)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	c:RegisterEffect(e3)
+	local e3a=e3:Clone()
+	e3a:SetCode(EFFECT_EXTRA_RELEASE_NONSUM)
+	e3a:SetCountLimit(1)
+	e3a:SetValue(s.relval)
+	c:RegisterEffect(e3a)
+end
+s.listed_series={SET_RIKKA}
+function s.setcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,SET_RIKKA),tp,LOCATION_MZONE,0,1,nil)
+end
+function s.setfilter(c)
+	return c:IsSetCard(SET_RIKKA) and c:IsSpellTrap() and not c:IsType(TYPE_FIELD) and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local sg=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #sg>0 then
+		Duel.SSet(tp,sg)
+	end
+	-- Cannot Special Summon, except Plant monsters
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(function(_,c) return not c:IsRace(RACE_PLANT) end)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.relval(e,re,r,rp)
+	return re and re:IsActivated() and (r&REASON_COST)~=0 and re:GetHandler():IsSetCard(SET_RIKKA)
+end

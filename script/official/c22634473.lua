293ac1c85fc5dbcdaa8d48d505cbc5ecae04c814
Naim <naimsantos45@gmail.com>
2023-09-01 14:17:43 +0000

    "Destined Rivals" fix
    It was negating the effects of any monster the opponent controls, instead of only face-up monsters

---
+++ script/official/c22634473.lua
@@ -0,0 +1,36 @@
+--対峙する宿命
+--Destined Rivals
+--Scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Negate the effects of face-up monsters the opponent controls
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={CARD_BLUEEYES_W_DRAGON,CARD_DARK_MAGICIAN}
+function s.filter(c)
+	return c:IsFaceup() and c:IsCode(CARD_BLUEEYES_W_DRAGON,CARD_DARK_MAGICIAN)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_ONFIELD,0,1,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsNegatableMonster,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,nil,1,1-tp,LOCATION_MZONE)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsNegatableMonster,tp,0,LOCATION_MZONE,nil)
+	local c=e:GetHandler()
+	for tc in g:Iter() do
+		tc:NegateEffects(c,RESET_PHASE|PHASE_END)
+	end
+end

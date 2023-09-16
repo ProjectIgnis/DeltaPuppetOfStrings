8753105bfba5adb91be754d4d336fb3ae6d933de
Naim <naimsantos45@gmail.com>
2023-09-16 15:10:52 +0000

    Update "Void Expansion"
    The effect that prevents battle and effect targets was not considering monsters that do not have level

---
+++ script/official/c34822850.lua
@@ -0,0 +1,72 @@
+--煉獄の氾爛
+--Void Expansion
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Special Summon 1 "Infernoid Token" during your Standby Phase
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(function(e,tp) return Duel.IsTurnPlayer(tp) end)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	--Custom effect for cost modification
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(LOCATION_HAND+LOCATION_GRAVE,0)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,SET_INFERNOID))
+	e3:SetCode(id)
+	c:RegisterEffect(e3)
+	--"Infernoid" monsters, except the ones with the highest level, cannot be targeted by opponent's effects or battle
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetTargetRange(0,LOCATION_MZONE)
+	e4:SetValue(s.atlimit)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e5:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e5:SetRange(LOCATION_FZONE)
+	e5:SetTargetRange(LOCATION_MZONE,0)
+	e5:SetTarget(s.tglimit)
+	e5:SetValue(aux.tgoval)
+	c:RegisterEffect(e5)
+end
+s.listed_names={id+1} --Infernoid Token
+s.listed_series={SET_INFERNOID}
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,SET_INFERNOID,TYPES_TOKEN,0,0,1,RACE_FIEND,ATTRIBUTE_FIRE) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		or not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,SET_INFERNOID,TYPES_TOKEN,0,0,1,RACE_FIEND,ATTRIBUTE_FIRE) then return end
+	local token=Duel.CreateToken(tp,id+1)
+	Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+end
+function s.filter(c,lv)
+	return c:IsFaceup() and c:IsSetCard(SET_INFERNOID) and c:GetLevel()>lv
+end
+function s.atlimit(e,c)
+	return c:IsFaceup() and c:IsSetCard(SET_INFERNOID) and (not c:HasLevel()
+		or Duel.IsExistingMatchingCard(s.filter,c:GetControler(),LOCATION_MZONE,0,1,nil,c:GetLevel()))
+end
+function s.tglimit(e,c)
+	return c:IsSetCard(SET_INFERNOID) and (not c:HasLevel()
+		or Duel.IsExistingMatchingCard(s.filter,c:GetControler(),LOCATION_MZONE,0,1,nil,c:GetLevel()))
+end

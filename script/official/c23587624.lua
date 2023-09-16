d49ecf4e5b0dbeae2e508d01500ec1490c4fb211
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-09-16 08:31:02 +0000

    "A Wild Monster Appears!" fix
    The summoning restriction and no damage effects should be applied even if you don't have a free Monster Zone on resolution + general script polishing and updates.

---
+++ script/official/c23587624.lua
@@ -0,0 +1,73 @@
+--青天の霹靂
+--A Wild Monster Appears!
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon 1 Level 10 or lower monster that cannot be Normal Summoned/Set from your hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(function(e,tp) return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0 and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 end)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.spfilter(c,e,tp)
+	return c:GetOriginalLevel()<=10 and not c:IsSummonableCard() and c:IsMonster()
+		and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp):GetFirst()
+		if sc and Duel.SpecialSummonStep(sc,0,tp,tp,true,false,POS_FACEUP) then
+			--Unaffected by the effects of your other cards
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+			e1:SetCode(EFFECT_IMMUNE_EFFECT)
+			e1:SetRange(LOCATION_MZONE)
+			e1:SetValue(function(_e,_re) return _re:GetOwnerPlayer()==tp and _e:GetHandler()~=_re:GetHandler() end)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+			sc:RegisterEffect(e1,true)
+			local reset_ct=(Duel.IsTurnPlayer(tp) or Duel.GetCurrentPhase()==PHASE_END) and 2 or 1
+			--Shuffle it into the Deck during your opponent's next End Phase
+			aux.DelayedOperation(sc,PHASE_END,id,e,tp,function(ag) Duel.SendtoDeck(ag,nil,SEQ_DECKSHUFFLE,REASON_EFFECT) end,function() return Duel.IsTurnPlayer(1-tp) end,nil,reset_ct,aux.Stringid(id,1))
+		end
+		Duel.SpecialSummonComplete()
+	end
+	--Cannot Normal Summon/Set or Special Summon monsters
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_MSET)
+	Duel.RegisterEffect(e2,tp)
+	local e3=e1:Clone()
+	e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	Duel.RegisterEffect(e3,tp)
+	--Your opponent takes no damage
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetCode(EFFECT_CHANGE_DAMAGE)
+	e4:SetTargetRange(0,1)
+	e4:SetValue(0)
+	e4:SetReset(RESET_PHASE|PHASE_END)
+	Duel.RegisterEffect(e4,tp)
+	local e5=e4:Clone()
+	e5:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+	Duel.RegisterEffect(e5,tp)
+end

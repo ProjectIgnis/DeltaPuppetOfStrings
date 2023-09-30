e1c285bf88ea80ec198e589b90a32e7c843079ec
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-09-30 14:21:41 +0000

    "Dark Beckoning Beast" fix
    Shouldn't be able to use the additional Normal Summon for monsters with ? ATK or DEF.

---
+++ script/official/c81034083.lua
@@ -0,0 +1,41 @@
+--暗黒の招来神
+--Dark Beckoning Beast
+--Scripted by AlphaKretin
+local s,id=GetID()
+function s.initial_effect(c)
+	--Search 1 "Uria, Lord of Searing Flames", "Hamon, Lord of Striking Thunder", or "Raviel, Lord of Phantasms", OR 1 card that mentions any of those cards
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--Can Normal Summon 1 Fiend monster with 0 ATK/DEF in addition to your Normal Summon/Set
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_HAND|LOCATION_MZONE,0)
+	e2:SetTarget(function(e,_c) return c:IsRace(RACE_FIEND) and c:IsAttack(0) and c:IsDefense(0) end)
+	c:RegisterEffect(e2)
+end
+s.listed_names={69890967,6007213,32491822,id}
+function s.thfilter(c)
+	return (c:IsCode(69890967,6007213,32491822) or c:ListsCode(69890967,6007213,32491822)) and not c:IsCode(id) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end

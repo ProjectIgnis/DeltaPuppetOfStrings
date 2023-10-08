3386c0d84d94747779182a9fcac639e772b43f73
Yoshi80 <yoshi.katreuvin@gmail.com>
2023-10-08 07:14:03 +0000

    fixed Stock Buster Dragon

---
+++ script/rush/c160006016.lua
@@ -0,0 +1,40 @@
+-- 恐攻竜ストックバスター
+-- Stock Buster Dragon
+-- Scripted by Hatter
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Reduce ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(function(_,tp) return Duel.GetFieldGroupCountRush(tp,0,LOCATION_ONFIELD)>=5 end)
+	e1:SetCost(s.atkcost)
+	e1:SetTarget(s.atktg)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
+end
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,nil) end
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsLevelBelow,7),tp,0,LOCATION_MZONE,1,nil) end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	-- Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,1,nil)
+	if Duel.SendtoGrave(g,REASON_COST)==0 then return end
+	-- Effect
+	local c=e:GetHandler()
+	local ag=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsLevelBelow,7),tp,0,LOCATION_MZONE,nil)
+	for tc in ag:Iter() do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-1500)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end

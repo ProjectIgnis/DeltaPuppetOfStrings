2dce56c634c0dcb94d0bc1422a9251958bc06be8
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-10-10 22:00:47 +0000

    "Fairy Tail Tales" fix
    Fixed an issue where the player would be unable to Tribute Summon using its effect if all their Main Monster Zones were full + general polish.

---
+++ script/official/c91957038.lua
@@ -0,0 +1,71 @@
+--妖精の伝姫
+--Fairy Tail Tales
+--Logical Nonsense and DyXel
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Normal Summon/Set 1 Spellcaster with 1850 ATK
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.nscost)
+	e2:SetTarget(s.nstg)
+	e2:SetOperation(s.nsop)
+	c:RegisterEffect(e2)
+	--The first time each turn you would take damage while you control a Spellcaster with 1850 ATK, you take no damage
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_CHANGE_DAMAGE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(1,0)
+	e3:SetCondition(s.damcon)
+	e3:SetValue(s.damval)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+	c:RegisterEffect(e4)
+end
+function s.nsfilter(c,tp)
+	return c:IsAttack(1850) and c:IsRace(RACE_SPELLCASTER) and c:CanSummonOrSet(true,nil) and not c:IsPublic()
+		and not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,c:GetCode()),tp,LOCATION_MZONE,0,1,nil)
+end
+function s.nscost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.nsfilter,tp,LOCATION_HAND,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local g=Duel.SelectMatchingCard(tp,s.nsfilter,tp,LOCATION_HAND,0,1,1,nil,tp)
+	Duel.ConfirmCards(1-tp,g)
+	Duel.ShuffleHand(tp)
+	Duel.SetTargetCard(g)
+end
+function s.nstg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.nsop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SummonOrSet(tp,tc,true,nil)
+	end
+end
+function s.damfilter(c)
+	return c:GetBaseAttack()==1850 and c:IsRace(RACE_SPELLCASTER) and c:IsFaceup()
+end
+function s.damcon(e)
+	return not e:GetHandler():HasFlagEffect(id) and Duel.IsExistingMatchingCard(s.damfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+function s.damval(e,re,val,r,rp,rc)
+	if r&(REASON_BATTLE|REASON_EFFECT)>0 then
+		e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END,0,1)
+		Duel.Hint(HINT_CARD,0,id)
+		return 0
+	end
+	return val
+end

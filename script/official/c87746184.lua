4ef2449684eeb915460099f62fe0f7a979b9d6bc
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-10-23 08:51:21 +0000

    "Albion the Branded Dragon" fix
    Fixed an issue where the player would be unable to Set a Field Spell with its effect if all their Spell & Trap Zones were occupied or unusable + general modernisation.

---
+++ script/official/c87746184.lua
@@ -0,0 +1,70 @@
+--烙印竜アルビオン
+--Albion the Branded Dragon
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Fusion Summon procedure
+	Fusion.AddProcMix(c,true,true,CARD_ALBAZ,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_LIGHT))
+	--Fusion Summon 1 Level 8 or lower Fusion Monster
+	local params={fusfilter=s.fusfilter,matfilter=Card.IsAbleToRemove,extrafil=s.fextra,extraop=Fusion.BanishMaterial,extratg=s.extratg}
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION) end)
+	e1:SetTarget(Fusion.SummonEffTG(params))
+	e1:SetOperation(Fusion.SummonEffOP(params))
+	c:RegisterEffect(e1)
+	--Add to your hand or Set 1 "Branded" Spell/Trap from your Deck
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_PHASE+PHASE_END)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.thsetcon)
+	e2:SetTarget(s.thsettg)
+	e2:SetOperation(s.thsetop)
+	c:RegisterEffect(e2)
+end
+s.listed_names={CARD_ALBAZ,id}
+s.listed_series={SET_BRANDED}
+function s.fusfilter(c)
+	return c:IsLevelBelow(8) and not c:IsCode(id)
+end
+function s.fextra(e,tp,mg)
+	if not Duel.IsPlayerAffectedByEffect(tp,CARD_SPIRIT_ELIMINATION) then
+		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
+	end
+	return nil
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,LOCATION_HAND|LOCATION_ONFIELD|LOCATION_GRAVE)
+end
+function s.thsetcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetTurnID()==Duel.GetTurnCount() and not c:IsReason(REASON_RETURN)
+end
+function s.thsetfilter(c)
+	return c:IsSetCard(SET_BRANDED) and c:IsSpellTrap() and (c:IsAbleToHand() or c:IsSSetable())
+end
+function s.thsettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thsetfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thsetop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
+	local sc=Duel.SelectMatchingCard(tp,s.thsetfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if not sc then return end
+	aux.ToHandOrElse(sc,tp,
+		function(sc) return sc:IsSSetable() end,
+		function(sc) Duel.SSet(tp,sc) end,
+		aux.Stringid(id,3)
+	)
+end

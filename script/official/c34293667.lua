b853d5459f2ccde9b58231a2d1a1c0a782b80f79
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-10-30 10:48:26 +0000

    "Ice Barrier" fixes
    - Now checks that the monster is face-up and that at least 1 of the 3 effects can be applied to it to activate the card.
    - Now checks that the opponent is still the controller of card and that it's still the same face-up card on resolution before applying the effects.
    - Added hint timings to its GY effect.
    - General script polishing and modernisation.

---
+++ script/official/c34293667.lua
@@ -0,0 +1,93 @@
+--氷結界
+--Ice Barrier
+--Scripted by fiftyfour
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--Send 1 Level 5 or higher WATER monster from your Deck to the GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetHintTiming(0,TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(aux.selfbanishcost)
+	e2:SetTarget(s.tgtg)
+	e2:SetOperation(s.tgop)
+	c:RegisterEffect(e2)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local _,bc=Duel.GetBattleMonster(tp)
+	if chk==0 then return bc and bc:IsFaceup() and (bc:IsAttackAbove(1) or bc:IsNegatableMonster()
+		or not bc:IsHasEffect(EFFECT_CANNOT_CHANGE_POSITION)) end
+	Duel.SetTargetCard(bc)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,bc,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,bc,1,tp,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local bc=Duel.GetFirstTarget()
+	if not (bc:IsRelateToEffect(e) and bc:IsFaceup() and bc:IsRelateToBattle() and bc:IsControler(1-tp)) then return end
+	local c=e:GetHandler()
+	--Negate its effects
+	bc:NegateEffects(c)
+	--Change its ATK to 0
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(3313)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+	e1:SetValue(0)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	bc:RegisterEffect(e1)
+	--Cannot change its battle position
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+	bc:RegisterEffect(e2)
+end
+function s.tgfilter(c)
+	return c:IsLevelAbove(5) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsAbleToGrave()
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.thfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsAbleToHand()
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=Duel.IsTurnPlayer(tp) and 2 or 1
+	--Cannot Special Summon monsters, except WATER monsters
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(function(_e,_c) return not _c:IsAttribute(ATTRIBUTE_WATER) end)
+	e1:SetReset(RESET_PHASE|PHASE_END|RESET_SELF_TURN,ct)
+	Duel.RegisterEffect(e1,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local tgc=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if not (tgc and Duel.SendtoGrave(tgc,REASON_EFFECT)>0 and tgc:IsLocation(LOCATION_GRAVE)) then return end
+	local thg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,nil)
+	if #thg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=thg:Select(tp,1,1,nil)
+		if #sg>0 then
+			Duel.BreakEffect()
+			Duel.SendtoHand(sg,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,sg)
+		end
+	end
+end

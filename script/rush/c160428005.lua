30c97266ed5769cc68785aa38038403852b05f97
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-12-08 19:35:51 +0000

    "Magician of Dark Sevens" fix
    Its ATK increasing effect wasn't being negated by "Darkness Galactica Oblivion".

---
+++ script/rush/c160428005.lua
@@ -0,0 +1,32 @@
+--マジシャン・オブ・ダークセブンス
+--Magician of Dark Sevens
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Fusion Material
+	Fusion.AddProcMix(c,true,true,CARD_SEVENS_ROAD_MAGICIAN,160428006)
+	--Increase ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.atkval)
+	c:RegisterEffect(e1)
+	--No battle damage from your other monsters
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(function(e,c) return c~=e:GetHandler() end)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+end
+function s.atkfilter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_SPELLCASTER)
+end
+function s.atkval(e,c)
+	return Duel.GetMatchingGroupCount(s.atkfilter,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil)*300
+end

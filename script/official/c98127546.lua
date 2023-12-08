5b803d875532b70194e58a248652d1aa254e2d1a
pyrQ <30827955+pyrQ@users.noreply.github.com>
2023-12-08 17:09:20 +0000

    "Underworld Goddess of the Closed World" update
    Updated to handle the interaction with "Moon of the Closed Sky" + overall small polishing and updates.

---
+++ script/official/c98127546.lua
@@ -0,0 +1,109 @@
+--閉ザサレシ世界ノ冥神
+--Underworld Goddess of the Closed World
+--Scripted by DyXel and Edo9300
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Link Summon procedure
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),4)
+	--You can also use 1 monster your opponent controls as material to Link Summon this card
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetProperty(EFFECT_FLAG_PLAYER_TARGET|EFFECT_FLAG_CANNOT_DISABLE|EFFECT_FLAG_SET_AVAILABLE)
+	e0:SetCode(EFFECT_EXTRA_MATERIAL)
+	e0:SetRange(LOCATION_EXTRA)
+	e0:SetTargetRange(1,1)
+	e0:SetOperation(s.extracon)
+	e0:SetValue(s.extraval)
+	c:RegisterEffect(e0)
+	--Negate the effects of all face-up monsters your opponent controls
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK) end)
+	e1:SetTarget(s.distg)
+	e1:SetOperation(s.disop)
+	c:RegisterEffect(e1)
+	--Unaffected by your opponent's activated effects, unless they target this card
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.immval)
+	c:RegisterEffect(e2)
+	--Negate the activation of an opponent's effect that includes Special Summoning a monster(s) from the GY
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_NEGATE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.spnegcon)
+	e3:SetTarget(s.spnegtg)
+	e3:SetOperation(function(_,_,_,_,ev) Duel.NegateActivation(ev) end)
+	c:RegisterEffect(e3)
+end
+s.curgroup=nil
+function s.closed_sky_filter(c)
+	return not (c:HasFlagEffect(100200249) and #c:GetCardTarget()>0)
+end
+function s.extracon(c,e,tp,sg,mg,lc,og,chk)
+	if not s.curgroup then return true end
+	local g=s.curgroup:Filter(s.closed_sky_filter,nil)
+	return #(sg&g)<2
+end
+function s.extraval(chk,summon_type,e,...)
+	if chk==0 then
+		local tp,sc=...
+		if summon_type~=SUMMON_TYPE_LINK or sc~=e:GetHandler() then
+			return Group.CreateGroup()
+		else
+			s.curgroup=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+			s.curgroup:KeepAlive()
+			return s.curgroup
+		end
+	elseif chk==2 then
+		if s.curgroup then
+			s.curgroup:DeleteGroup()
+		end
+		s.curgroup=nil
+	end
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsNegatableMonster,tp,0,LOCATION_MZONE,1,nil) end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsNegatableMonster,tp,0,LOCATION_MZONE,nil)
+	if #g==0 then return end
+	local c=e:GetHandler()
+	for tc in g:Iter() do
+		--Negate its effects
+		tc:NegateEffects(c)
+	end
+end
+function s.immval(e,re)
+	local c=e:GetHandler()
+	if not (re:IsActivated() and c:IsSummonType(SUMMON_TYPE_LINK) and e:GetOwnerPlayer()==1-re:GetOwnerPlayer()) then return false end
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return true end
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	return not g or not g:IsContains(c)
+end
+function s.spnegcon(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) or rp==tp
+		or not Duel.IsChainNegatable(ev) then return false end
+	local ex1,tg1,ct1,p1,loc1=Duel.GetOperationInfo(ev,CATEGORY_SPECIAL_SUMMON)
+	local ex2,tg2,ct2,p2,loc2=Duel.GetPossibleOperationInfo(ev,CATEGORY_SPECIAL_SUMMON)
+	local res1=((loc1 or 0)&LOCATION_GRAVE>0) or (tg1 and tg1:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE))
+	local res2=((loc2 or 0)&LOCATION_GRAVE>0) or (tg2 and tg2:IsExists(Card.IsLocation,1,nil,LOCATION_GRAVE))
+	return res1 or res2
+end
+function s.spnegtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+end

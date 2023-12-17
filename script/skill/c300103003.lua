7971fc719999762a2b88475119904ba08901eb4c
Naim <naimsantos45@gmail.com>
2023-12-17 18:07:20 +0000

    "Bandit" fix
    It was checking for a card anywhere in the opponent's field instead of just the spell/trap zone

---
+++ script/skill/c300103003.lua
@@ -0,0 +1,23 @@
+--Bandit
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	--opd check
+	if Duel.GetFlagEffect(ep,id)>0 then return end
+	--condition
+	return aux.CanActivateSkill(tp) and Duel.GetLP(tp)<=1500
+		and Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_STZONE,1,nil)
+end
+function s.filter(c)
+	return c:IsFacedown() and c:IsAbleToChangeControler()
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	--opd register
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_STZONE,1,1,nil)
+	Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_SZONE,POS_FACEDOWN,true)
+end

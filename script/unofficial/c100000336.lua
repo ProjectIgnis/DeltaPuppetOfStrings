74483dd587f547b3d9c1f241f31488174f535242
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-01-26 02:36:01 +0000

    Update "Hot Red Dragon Archfiend (Manga)"
    Should not be able to attack without destroying a monster by its first effect/minor script modernization update.

---
+++ script/unofficial/c100000336.lua
@@ -0,0 +1,38 @@
+--琰魔竜 レッド・デーモン (Manga)
+--Hot Red Dragon Archfiend (Manga)
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Synchro Summon
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99
+	--Destroy all other Attack Position monsters
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--Cannot attack unless it destroys a monster this turn
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e2:SetCondition(function(e) return e:GetHandler():GetFlagEffect(id)==0 end)
+	c:RegisterEffect(e2)	
+end
+function s.filter(c)
+	return c:IsAttackPos() and c:IsDestructable()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler()) end
+	local sg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,#sg,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local sg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
+	if Duel.Destroy(sg,REASON_EFFECT)>0 then
+		e:GetHandler():RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END,0,1)
+	end
+end

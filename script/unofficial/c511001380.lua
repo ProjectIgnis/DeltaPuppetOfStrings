123b87f7359c3b62fa660d641dedce3e5d992cdb
Naim <naimsantos45@gmail.com>
2024-01-27 19:32:42 +0000

    "Explosion Fuse" update
    - Fixed an incorrect effect number used with a property in one of the effects
    - general script cleanup

---
+++ script/unofficial/c511001380.lua
@@ -0,0 +1,84 @@
+--導火線
+--Explosion Fuse
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon 1 "Power Bombard"
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_BATTLE_DESTROYED)
+	e1:SetCondition(s.con)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--Destroy this card when the monster leaves the field
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+end
+s.listed_names={511001379} --Power Bombard
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetHandler():GetFirstCardTarget()
+	if tc and tc:IsLocation(LOCATION_MZONE) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(Card.IsPreviousControler,1,nil,1-tp)
+end
+function s.spfilter(c,e,tp)
+	return c:IsCode(511001379) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_ATTACK)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp):GetFirst()
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_ATTACK)>0 then
+		Duel.Equip(tp,c,tc)
+		--Add Equip limit
+		local e1=Effect.CreateEffect(tc)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		e1:SetValue(function(e,cc) return e:GetOwner()==cc end)
+		c:RegisterEffect(e1)
+		--Destroy the equipped monster and inflict 1000 Damage
+		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(aux.Stringid(id,1))
+		e2:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+		e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+		e2:SetProperty(0,EFFECT_FLAG2_CONTINUOUS_EQUIP)
+		e2:SetRange(LOCATION_SZONE)
+		e2:SetCountLimit(1)
+		e2:SetCondition(function(e,tp) return Duel.IsTurnPlayer(tp) end)
+		e2:SetTarget(s.damtg)
+		e2:SetOperation(s.damop)
+		e2:SetReset(RESET_PHASE|PHASE_END,Duel.IsTurnPlayer(tp) and 3 or 2)
+		c:RegisterEffect(e2)
+	end
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local eq=e:GetHandler():GetEquipTarget()
+	if chk==0 then return eq end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,eq,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1000)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local eq=e:GetHandler():GetEquipTarget()
+	if not eq then return end
+	if Duel.Destroy(eq,REASON_EFFECT)>0 then
+		Duel.Damage(1-tp,1000,REASON_EFFECT)
+	end
+end

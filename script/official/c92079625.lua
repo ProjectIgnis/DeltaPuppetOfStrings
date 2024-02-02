1c01e20148521b03a6aad80c4b1957d17a51f6ce
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-02-02 18:20:18 +0000

    "Helshaddoll Hollow" fix
    Shouldn't send a monster from the Extra Deck to the GY if the target isn't face-up on the field anymore when this effect resolves.

---
+++ script/official/c92079625.lua
@@ -0,0 +1,63 @@
+--影依の炎核 ヴォイド
+--Helshaddoll Hollow
+--Scripted by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--Send 1 "Shaddoll" monster from your Extra Deck to the GY and banish 1 opponent's face-up monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_FLIP+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.rmtg)
+	e1:SetOperation(s.rmop)
+	c:RegisterEffect(e1)
+	--Send cards from the top of your Deck to the GY, equal to the number of different original Attributes among the monsters on the field
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DECKDES)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(function(e) return e:GetHandler():IsReason(REASON_EFFECT) end)
+	e2:SetTarget(s.tgtg)
+	e2:SetOperation(s.tgop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_SHADDOLL}
+function s.rmfilter(c,tp)
+	return c:IsFaceup() and Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_EXTRA,0,1,nil,c:GetAttribute())
+end
+function s.tgfilter(c,att)
+	return c:IsSetCard(SET_SHADDOLL) and c:IsAbleToGrave() and c:IsAttribute(att)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.rmfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.rmfilter,tp,0,LOCATION_MZONE,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,s.rmfilter,tp,0,LOCATION_MZONE,1,1,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_EXTRA)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,tp,0)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not (tc:IsRelateToEffect(e) and tc:IsFaceup()) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local sc=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_EXTRA,0,1,1,nil,tc:GetAttribute()):GetFirst()
+	if sc and Duel.SendtoGrave(sc,REASON_EFFECT)>0 and sc:IsLocation(LOCATION_GRAVE) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	end
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local attct=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil):GetClassCount(Card.GetOriginalAttribute)
+	if chk==0 then return attct>0 and Duel.IsPlayerCanDiscardDeck(tp,attct) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,attct)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local attct=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil):GetClassCount(Card.GetOriginalAttribute)
+	if attct>0 and Duel.IsPlayerCanDiscardDeck(tp,attct) then
+		Duel.DiscardDeck(tp,attct,REASON_EFFECT)
+	end
+end

302e5489148aba88135fdb32671d334d23eb11dc
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-02-02 17:59:55 +0000

    "Elemental HERO Ocean" fix
    Shouldn't return the target to the hand if it's not a correct target anymore on resolution.

---
+++ script/official/c37195861.lua
@@ -0,0 +1,35 @@
+--Ｅ・ＨＥＲＯ オーシャン
+--Elemental HERO Ocean
+local s,id=GetID()
+function s.initial_effect(c)
+	--Return 1 "HERO" monster you control or in your GY to the hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(function(e,tp) return Duel.IsTurnPlayer(tp) end)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+end
+s.listed_series={SET_HERO}
+function s.thfilter(c)
+	return c:IsSetCard(SET_HERO) and c:IsMonster() and c:IsAbleToHand() and c:IsFaceup()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE|LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,tp,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsControler(tp) and s.thfilter(tc) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end

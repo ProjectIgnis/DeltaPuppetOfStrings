9422d86cbc9f4989b3e97d8adcd91d27c5cede02
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-02-16 03:28:27 +0000

    Update "Summon Cloud (Anime)"
    Following changes:
    - Summon effect is non-OPT
    - Should only be able to summon Level 4 or lower "Cloudians" from hand
    - Script changes to reflect modernization updates

---
+++ script/unofficial/c511001924.lua
@@ -0,0 +1,54 @@
+--召喚雲 (Anime)
+--Summon Cloud (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Special Summon Level 4 or lower "Cloudian" from your hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(55375684,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+	--Discard 1 card on your Standby Phase or destroy this card
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(function(_,tp) return Duel.GetTurnPlayer()==tp end)
+	e3:SetOperation(s.mtop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={SET_CLOUDIAN}
+function s.spfilter(c,e,tp)
+	return c:IsLevelBelow(4) and c:IsSetCard(SET_CLOUDIAN) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.mtop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) 
+		and Duel.SelectYesNo(tp,aux.Stringid(79850798,1)) then
+		Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
+	else
+		Duel.Destroy(e:GetHandler(),REASON_RULE)
+	end
+end

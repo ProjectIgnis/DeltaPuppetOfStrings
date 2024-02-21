486da6933c6a1eb41221b610398aa7daa5f3394e
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-02-21 03:14:51 +0000

    Fix "Champion's Majesty"
    Should gain ATK for destroying by its effect as well as by battle/script modernization.

---
+++ script/unofficial/c513000067.lua
@@ -0,0 +1,110 @@
+--王者の威光
+--Champion's Majesty
+Duel.LoadScript("c420.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetCategory(CATEGORY_EQUIP)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	e0:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
+	e0:SetCondition(s.condition)
+	e0:SetTarget(s.target)
+	e0:SetOperation(s.operation)
+	c:RegisterEffect(e0)
+	--Equip limit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_EQUIP_LIMIT)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--Negate the effects of all cards on your opponent's field
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(0,LOCATION_ONFIELD)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetCode(EFFECT_DISABLE)
+	c:RegisterEffect(e2)
+	--Equipped monster gains 500 ATK for each monster it destroys (battle)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(63851864,0))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_BATTLE_DESTROYING)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCondition(s.atkbcon)
+	e3:SetOperation(s.atkbop)
+	c:RegisterEffect(e3)
+	--Equipped monster gains 500 ATK for each monster it destroys (effect)
+	local e4=e3:Clone()
+	e4:SetCode(EVENT_DESTROYED)
+	e4:SetCondition(s.atkecon)
+	e4:SetOperation(s.atkeop)
+	c:RegisterEffect(e4)
+	--This card's effects cannot be negated
+    	local e5=Effect.CreateEffect(c)
+    	e5:SetType(EFFECT_TYPE_SINGLE)
+    	e5:SetCode(EFFECT_CANNOT_DISABLE)
+    	c:RegisterEffect(e5)
+	
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(Card.IsChampion,tp,LOCATION_GRAVE,0,3,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Equip(tp,c,tc)
+	end
+end
+function s.atkbcon(e,tp,eg,ep,ev,re,r,rp)
+	local ec=eg:GetFirst()
+	local bc=ec:GetBattleTarget()
+	return e:GetHandler():GetEquipTarget()==eg:GetFirst() and bc:IsReason(REASON_BATTLE)
+end
+function s.atkbop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local tc=c:GetEquipTarget()
+	if tc then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		e1:SetValue(500)
+		tc:RegisterEffect(e1)
+	end
+end
+function s.atkefilter(c,e,re)
+	local rc=e:GetHandler():GetEquipTarget()
+	return c:GetReason&(REASON_DESTROY|REASON_EFFECT)==(REASON_DESTROY|REASON_EFFECT) and re:GetHandler()==rc
+end
+function s.atkecon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.atkefilter,1,nil,e,re)
+end
+function s.atkeop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local tc=c:GetEquipTarget()
+	local dg=eg:Filter(s.atkefilter,nil,e,re)
+	if tc and #dg>0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		e1:SetValue(#dg*500)
+		tc:RegisterEffect(e1)
+	end
+end

3eb95ec5e92499aa41cde2abf8f78b95687b04bd
Yoshi80 <yoshi.katreuvin@gmail.com>
2024-02-22 23:01:35 +0000

    fixed rush cards that change to face-down Defense Position
    should all use the same filter to interact correctly with Maximum monsters

--- script/rush/c160009065.lua
+++ script/rush/c160009065.lua
@@ -1,11 +1,10 @@
---エクスキューティー・キャッチ！
---Executie Catch!
---scripted by YoshiDuels
+--聖塔の危機
+--Crisis at the Sacred Tower
+--scripted by pyrQ
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_POSITION)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
@@ -13,21 +12,12 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	c:RegisterEffect(e2)
-end
-function s.filter1(c,tp)
-	return c:IsSummonPlayer(1-tp) and c:IsLocation(LOCATION_MZONE)
-end
-function s.cfilter(c)
-	return c:IsFaceup() and c:IsLevel(6) and c:IsDefense(500)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.filter1,1,nil,tp) and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+	return ep==1-tp and eg:GetFirst():IsLevelAbove(7)
 end
 function s.filter(c)
-	return c:IsFaceup() and c:IsLevelBelow(8) and c:IsCanTurnSet() and c:IsCanChangePositionRush()
+	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and c:IsLevelBelow(8) and c:IsCanTurnSet() and c:IsCanChangePositionRush()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
--- script/rush/c160010064.lua
+++ script/rush/c160010064.lua
@@ -1,9 +1,9 @@
---ライステラス・クライシス
---Rice Terrace Crisis
---scripted by YoshiDuels
+-- 落とし蓋
+-- Trap Lid
+-- Scripted by Hatter
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Change up 2 monsters to face-down Defense Position
+	-- Change 2 monsters to face-down Defense Position
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_POSITION)
@@ -16,32 +16,24 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.posconfilter(c,tp)
-	return c:IsFaceup() and c:IsSummonPlayer(1-tp) and c:IsSummonLocation(LOCATION_GRAVE|LOCATION_HAND)
+	return c:IsFaceup() and c:IsSummonPlayer(1-tp) and c:IsSummonLocation(LOCATION_GRAVE)
 end
 function s.poscon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.posconfilter,1,nil,tp)
 end
 function s.posfilter(c)
-	return c:IsFaceup() and c:IsLevelBelow(12) and c:IsCanTurnSet() and c:IsCanChangePositionRush()
+	return c:IsFaceup() and c:IsLevelBelow(8) and c:IsCanTurnSet() and c:IsCanChangePositionRush()
 end
 function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.posfilter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,nil,1,tp,0)
 end
-function s.posfilter2(c)
-	return c:IsCanTurnSet() and c:IsCanChangePositionRush()
-end
 function s.posop(e,tp,eg,ep,ev,re,r,rp)
 	-- Effect
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-	local g=Duel.GetMatchingGroup(aux.FaceupFilter(s.posfilter2),tp,0,LOCATION_MZONE,nil)
+	local g=Duel.SelectMatchingCard(tp,s.posfilter,tp,0,LOCATION_MZONE,1,2,nil)
 	if #g>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-		local sg=aux.SelectUnselectGroup(g,e,tp,1,2,s.rescon,1,tp,HINTMSG_POSCHANGE)
-		Duel.HintSelection(sg,true)
-		Duel.ChangePosition(sg,POS_FACEDOWN_DEFENSE,0,POS_FACEDOWN_DEFENSE,0)
+		Duel.HintSelection(g,true)
+		Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE,0,POS_FACEDOWN_DEFENSE,0)
 	end
-end
-function s.rescon(sg,e,tp,mg)
-	return sg:GetSum(Card.GetLevel)<=12
 end

7b6629fd19cbb5b9ae39b7ea7a478991eebd074f
Naim <naimsantos45@gmail.com>
2024-03-20 10:53:36 +0000

    "Trickstar Bella Madonna" fix
    - Fixed a bug where a non-Link monster that copied its effects would be able to use the effect to inflict damage
    - updated the conditions used by the first and second effect to better reflect the text.

---
+++ script/official/c41302052.lua
@@ -0,0 +1,56 @@
+--トリックスター・ベラマドンナ
+--Trickstar Bella Madonna
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Link Summon Procedure
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_TRICKSTAR),2)
+	--Unaffected by other card effects while it doesn't point to a monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	--e1:SetCondition(s.imcon) --handled in value for mid-resolution updating
+	e1:SetValue(s.immval)
+	c:RegisterEffect(e1)
+	--Inflict 200 damage to your opponent for each "Trickstar" monster in your GY with a different name
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.damcon)
+	e2:SetTarget(s.damtg)
+	e2:SetOperation(s.damop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_TRICKSTAR}
+function s.imcon(e)
+	local c=e:GetHandler()
+	return c:IsSummonType(SUMMON_TYPE_LINK) and c:GetLinkedGroup():FilterCount(Card.IsMonster,nil)==0
+end
+function s.immval(e,te)
+	return te:GetOwner()~=e:GetHandler() and te:IsActivated() and s.imcon(e) --condition handling for mid-resolution updating
+end
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsLinkMonster() and c:GetLinkedGroup():FilterCount(Card.IsMonster,nil)==0
+end
+function s.damfilter(c)
+	return c:IsSetCard(SET_TRICKSTAR) and c:IsMonster()
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.damfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	local g=Duel.GetMatchingGroup(s.damfilter,tp,LOCATION_GRAVE,0,nil)
+	local dam=g:GetClassCount(Card.GetCode)*200
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(dam)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local g=Duel.GetMatchingGroup(s.damfilter,tp,LOCATION_GRAVE,0,nil)
+	local dam=g:GetClassCount(Card.GetCode)*200
+	Duel.Damage(p,dam,REASON_EFFECT)
+end

546bf3d885d201faedcc8413edf6039681f592d7
Naim <naimsantos45@gmail.com>
2024-03-21 16:13:34 +0000

    "Dark World Brainwashing" update
    - The player should only be able to use the effect once per chain
    - Added a card hint when the effect resolves to identify that "Dark World Brainwashing"effect change

---
+++ script/official/c10131855.lua
@@ -0,0 +1,57 @@
+--暗黒界の洗脳
+--Dark World Brainwashing
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Change opponent's monster effect
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetCountLimit(1,0,EFFECT_COUNT_CODE_CHAIN)
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetRange(LOCATION_SZONE)
+	c:RegisterEffect(e3)
+end
+s.listed_series={SET_DARK_WORLD}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return ep==1-tp and re:IsMonsterEffect() and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>=3
+end
+function s.thfilter(c)
+	return c:IsFaceup() and c:IsSetCard(SET_DARK_WORLD) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,tp,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_HAND) then
+		local g=Group.CreateGroup()
+		Duel.ChangeTargetCard(ev,g)
+		Duel.ChangeChainOperation(ev,s.repop)
+	end
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,1-tp,id)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
+	if #g>0 then
+		local sg=g:RandomSelect(1-tp,1)
+		Duel.SendtoGrave(sg,REASON_EFFECT|REASON_DISCARD)
+	end
+end

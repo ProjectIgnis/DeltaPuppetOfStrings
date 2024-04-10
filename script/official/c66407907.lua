c35e5b726de885c3267faeeb2529b5c78adf1bde
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-04-10 12:25:26 +0000

    Couple of bug fixes
    - Blue Dragon Ninja: Shouldn't need to be in Attack Position to use its effect. Also, the "cannot attack" part should be applied even if the target is face-down on resolution.
    - Sunseed Twin: Should consider the MMZ that will be free after banishing the Link Monster from the field for the cost.

---
+++ script/official/c66407907.lua
@@ -0,0 +1,84 @@
+--聖種の天双芽
+--Sunseed Twin
+--Scripted by Eerie Code, anime script by Playmaker 772211 and Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon 1 Level 4 or lower Plant Normal Monster from your GY 
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.normalspcon)
+	e1:SetTarget(s.normalsptg)
+	e1:SetOperation(s.normalspop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--Special Summon 1 Plant Link Monster from your GY
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCost(s.linkspcost)
+	e3:SetTarget(s.linksptg)
+	e3:SetOperation(s.linkspop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={SET_SUNAVALON}
+function s.normalspconfilter(c)
+	return c:IsSetCard(SET_SUNAVALON) and c:IsLinkMonster() and c:IsFaceup() 
+end
+function s.normalspcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.normalspconfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.normalspfilter(c,e,tp)
+	return c:IsLevelBelow(4) and c:IsRace(RACE_PLANT) and c:IsType(TYPE_NORMAL)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.normalsptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.normalspfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.normalspfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.normalspfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,0)
+end
+function s.normalspop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.linkspcostfilter(c,tp)
+	return c:IsLinkMonster() and c:IsAbleToRemoveAsCost() and Duel.GetMZoneCount(tp,c)>0
+end
+function s.linkspcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToRemoveAsCost()
+		and Duel.IsExistingMatchingCard(s.linkspcostfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.linkspcostfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+	Duel.Remove((g+c),POS_FACEUP,REASON_COST)
+end
+function s.linkspfilter(c,e,tp)
+	return c:IsRace(RACE_PLANT) and c:IsLinkMonster() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,1,c,c:GetCode())
+end
+function s.linksptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.linkspfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end	
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.linkspop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.linkspfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end

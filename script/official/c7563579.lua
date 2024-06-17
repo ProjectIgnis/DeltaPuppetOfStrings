8af7f6dfdb1b089ab5f7e43d6f6d633a99fea13e
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-06-17 17:38:11 +0000

    "Performage Plushfire" errata script updates

---
+++ script/official/c7563579.lua
@@ -0,0 +1,71 @@
+--Ｅｍヒグルミ
+--Performage Plushfire
+local s,id=GetID()
+function s.initial_effect(c)
+	--Pendulum Summon procedure
+	Pendulum.AddProcedure(c)
+	--Special Summon this card from your Pendulum Zone, then take 500 damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_DESTROYED)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.selfspcon)
+	e1:SetTarget(s.selfsptg)
+	e1:SetOperation(s.selfspop)
+	c:RegisterEffect(e1)
+	--Special Summon 1 "Performage" monster from your hand or Deck, except "Performage Plushfire"
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) and (r&REASON_BATTLE|REASON_EFFECT)>0 end)
+	e2:SetTarget(s.hdsptg)
+	e2:SetOperation(s.hdspop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_PERFORMAGE}
+s.listed_names={id}
+function s.selfspconfilter(c,tp)
+	return c:IsPreviousSetCard(SET_PERFORMAGE) and c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousPosition(POS_FACEUP) and c:IsPreviousControler(tp)
+		and c:IsReason(REASON_BATTLE|REASON_EFFECT)
+end
+function s.selfspcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.selfspconfilter,1,nil,tp)
+end
+function s.selfsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,500)
+end
+function s.selfspop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		Duel.BreakEffect()
+		Duel.Damage(tp,500,REASON_EFFECT)
+	end
+end
+function s.hdspfilter(c,e,tp)
+	return c:IsSetCard(SET_PERFORMAGE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:IsCode(id)
+end
+function s.hdsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.hdspfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_DECK)
+end
+function s.hdspop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.hdspfilter,tp,LOCATION_HAND|LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end

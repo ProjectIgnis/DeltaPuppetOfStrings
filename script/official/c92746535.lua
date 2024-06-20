33598d9d2eb12bd354d4b3775e49d857fd22d3cc
Naim <naimsantos45@gmail.com>
2024-06-20 11:10:13 +0000

    "Luster Pendulum, The Dracoslayer" fix
    Fixed a bug where it would be unable to add to the hand cards that are always treated as other cards and similars (any card using an alias)

---
+++ script/official/c92746535.lua
@@ -0,0 +1,59 @@
+--竜剣士ラスターP
+--Luster Pendulum, The Dracoslayer
+local s,id=GetID()
+function s.initial_effect(c)
+	--Pendulum procedure
+	Pendulum.AddProcedure(c)
+	--Destroy a card in your Pendulum Zone and add 1 card with the same name from your Deck to your hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(s.thcon)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--Cannot be Fusion, Synchro or Xyz material
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+	e2:SetValue(s.splimit)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+	c:RegisterEffect(e3)
+	local e4=e2:Clone()
+	e4:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
+	c:RegisterEffect(e4)
+end
+s.listed_series={SET_DRACOSLAYER}
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(nil,tp,LOCATION_PZONE,0,1,e:GetHandler())
+end
+function s.thfilter(c,code)
+	return c:IsCode(code) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local sc=Duel.GetFirstMatchingCard(nil,tp,LOCATION_PZONE,0,e:GetHandler())
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,sc:GetCode()) end
+	Duel.SetTargetCard(sc)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sc,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,tc:GetCode())
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
+function s.splimit(e,c)
+	if not c then return false end
+	return not c:IsSetCard(SET_DRACOSLAYER)
+end

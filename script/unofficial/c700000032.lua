aa2b4a8c565be82798304297c7e211e141113b09
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-07-05 20:41:07 +0000

    Fix "Thorn Observer Zuma"
    Should be able to use a "Thorn Prisoner" Tuner as the "Thorn Prisoner" requirement/slight script polish

---
+++ script/unofficial/c700000032.lua
@@ -0,0 +1,150 @@
+--茨の戒人－ズーマ
+--Thorn Observer - Zuma
+--Scripted by Eerie Code
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Synchro Summon procedure
+	Synchro.AddProcedure(c,nil,1,1,aux.FilterBoolFunctionEx(Card.IsSetCard,0x556),1,1)
+	--Place Thorn Counters on monsters 
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_COUNTER)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO) end)
+	e1:SetTarget(s.pctg)
+	e1:SetOperation(s.pcop)
+	c:RegisterEffect(e1)
+	--Monsters with Thorn Counters cannot attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(function(e,c) return c:GetCounter(0x1104)~=0 end)
+	c:RegisterEffect(e2)
+	--Turn player takes 400 damage for each Thorn Counter on their field
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCountLimit(1)
+	e3:SetTarget(s.damtg)
+	e3:SetOperation(s.damop)
+	c:RegisterEffect(e3)
+	--Take no battle damage and Special Summon this card along with its Synchro Materials
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_BE_BATTLE_TARGET)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCost(s.nbcost)
+	e4:SetTarget(s.nbtg)
+	e4:SetOperation(s.nbop)
+	c:RegisterEffect(e4)
+	--Multiple Tuners
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_MATERIAL_CHECK)
+	e5:SetValue(s.valcheck)
+	c:RegisterEffect(e5)
+end
+function s.valcheck(e,c)
+	local g=c:GetMaterial()
+	if g:IsExists(Card.IsType,2,nil,TYPE_TUNER) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+		e1:SetCode(EFFECT_MULTIPLE_TUNERS)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD&~(RESET_TOFIELD)|RESET_PHASE|PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end
+function s.pctg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+end
+function s.pcop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	local tc=g:GetFirst()
+	while tc do
+		tc:AddCounter(0x1104,1)
+		tc=g:GetNext()
+	end
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local p=Duel.GetTurnPlayer()
+	local ct=Duel.GetCounter(p,LOCATION_ONFIELD,0,0x1104)
+	Duel.SetTargetPlayer(p)
+	Duel.SetTargetParam(ct*400)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,p,ct*400)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end
+function s.nbcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,400) end
+	Duel.PayLPCost(tp,400)
+end
+function s.mgfilter(c,e,tp,sync)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_GRAVE)
+		and c:GetReason()&0x80008==0x80008 and c:GetReasonCard()==sync
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and c:IsCanBeEffectTarget(e)
+end
+function s.nbtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	local mg=c:GetMaterial()
+	local ct=#mg
+	local sumtype=c:GetSummonType()
+	if chkc then return false end
+	if chk==0 then return sumtype==SUMMON_TYPE_SYNCHRO and ct>0 and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) 
+		and ct<=Duel.GetLocationCount(tp,LOCATION_MZONE) and mg:FilterCount(s.mgfilter,nil,e,tp,c)==ct end
+	Duel.SetTargetCard(mg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,mg,#mg,0,0)
+end
+function s.nbop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local g=tg:Filter(Card.IsRelateToEffect,nil,e)
+	if not c:IsRelateToEffect(e) or #g~=#tg then return end
+	local fid=c:GetFieldID()
+	local tc=g:GetFirst()
+	while tc do
+		tc:RegisterFlagEffect(id,RESETS_STANDARD_PHASE_END,0,1,fid)
+		tc=g:GetNext()
+	end
+	g:KeepAlive()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+	e1:SetOperation(s.damop2)
+	e1:SetReset(RESET_PHASE|PHASE_DAMAGE)
+	Duel.RegisterEffect(e1,tp)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_DAMAGE_STEP_END)
+	e2:SetOperation(s.spop)
+	e2:SetLabelObject(g)
+	e2:SetLabel(#g)
+	e2:SetValue(fid)
+	e2:SetReset(RESET_PHASE|PHASE_DAMAGE)
+	Duel.RegisterEffect(e2,tp)
+end
+function s.damop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ChangeBattleDamage(tp,0)
+end
+function s.spfilter(c,fid)
+	return c:GetFlagEffectLabel(id)==fid
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=e:GetLabelObject()
+	local tg=g:Filter(s.spfilter,nil,e:GetValue())
+	if not c:IsLocation(LOCATION_GRAVE) or not c:IsCanBeSpecialSummoned(e,0,tp,false,false) or #tg~=e:GetLabel() then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+	tg:AddCard(c)
+	Duel.SpecialSummon(tg,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+end

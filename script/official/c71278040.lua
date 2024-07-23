eaec49a93232a134acf690014d2a4ab6aaedd6b7
Naim <naimsantos45@gmail.com>
2024-07-23 12:37:31 +0000

    "Parallel eXceed" fix
    Fixed a bug in which it would remain in the player's hand even if the zone(s) the link monster pointed to were not available during the resolution of the effect (it should be sent to the GY, instead)

---
+++ script/official/c71278040.lua
@@ -0,0 +1,101 @@
+--パラレルエクシード
+--Parallel eXceed
+--Anime version scripted by Larry126
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon this card to a zone a Link monster points to
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.handspcon)
+	e1:SetTarget(s.handsptg)
+	e1:SetOperation(s.handspop)
+	c:RegisterEffect(e1)
+	--Special Summon 1 "Parallel eXceed" from your Deck
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	--Change its level, ATK and DEF if it is summoned by the effect of "Parallel eXceed"
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetCondition(s.condition)
+	e4:SetOperation(s.operation)
+	c:RegisterEffect(e4)
+end
+s.listed_names={id}
+function s.linkfilter(c,tp)
+	return c:IsSummonType(SUMMON_TYPE_LINK) and c:IsSummonPlayer(tp)
+end
+function s.handspcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.linkfilter,1,nil,tp)
+end
+function s.handsptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	local zones=eg:Filter(Card.IsSummonType,nil,SUMMON_TYPE_LINK):GetLinkedZone(tp)&ZONES_MMZ
+	if chk==0 then return zones~=0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP,tp,zones) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+end
+function s.handspop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local zones=eg:Filter(Card.IsSummonType,nil,SUMMON_TYPE_LINK):GetLinkedZone(tp)&ZONES_MMZ
+	if c:IsRelateToEffect(e) and zones~=0 then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP,zones)
+	end
+end
+function s.spfilter(c,e,tp)
+	return c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return re and re:GetHandler():IsCode(id)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Level becomes 4
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CHANGE_LEVEL)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(4)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
+	c:RegisterEffect(e1)
+	--ATK/DEF becomes halved
+	local e2=e1:Clone()
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_SET_BASE_ATTACK)
+	e2:SetValue(c:GetBaseAttack()/2)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_SET_BASE_DEFENSE)
+	e3:SetValue(c:GetBaseDefense()/2)
+	c:RegisterEffect(e3)
+end

adf50207bdb850e45c7f1b718274175468b5e113
Naim <naimsantos45@gmail.com>
2024-08-03 19:43:07 +0000

    "Ancient Gear Ballista" fix
    It should not be able to target a monster that has both its ATK and DEF equal to 0

---
+++ script/official/c10547580.lua
@@ -0,0 +1,85 @@
+--古代の機械弩士
+--Ancient Gear Ballista
+--scripted by Logical Nonsense
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Link Materials: 2 EARTH Machine monsters
+	Link.AddProcedure(c,s.matfilter,2,2)
+	--Add 1 "Ancient Gear" monster or "Geartown" from your Deck to your hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK) end)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--Destroy your S/T, and if you do, monster's ATK/DEF become 0
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+end
+s.listed_names={37694547} --"Geartown"
+s.listed_series={SET_ANCIENT_GEAR} 
+function s.matfilter(c,scard,sumtype,tp)
+	return c:IsRace(RACE_MACHINE,scard,sumtype,tp) and c:IsAttribute(ATTRIBUTE_EARTH,scard,sumtype,tp)
+end
+function s.thfilter(c)
+	return ((c:IsSetCard(SET_ANCIENT_GEAR) and c:IsMonster()) or c:IsCode(37694547)) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.atkdeffilter(c)
+	return c:IsFaceup() and (c:GetAttack()>0 or c:GetDefense()>0)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsSpellTrap,tp,LOCATION_ONFIELD,0,1,nil)
+		and Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g1=Duel.SelectTarget(tp,Card.IsSpellTrap,tp,LOCATION_ONFIELD,0,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+	local g2=Duel.SelectTarget(tp,s.atkdeffilter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g1,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g2,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g1,g2=Duel.GetFirstTarget()
+	local ex,g1=Duel.GetOperationInfo(0,CATEGORY_DESTROY)
+	local ex,g2=Duel.GetOperationInfo(0,CATEGORY_ATKCHANGE)
+	if g1:GetFirst():IsRelateToEffect(e) and Duel.Destroy(g1,REASON_EFFECT)>0 then
+		local tc=g2:GetFirst()
+		if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e1:SetValue(0)
+			e1:SetReset(RESETS_STANDARD_PHASE_END)
+			tc:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
+			tc:RegisterEffect(e2)
+		end
+	end
+end

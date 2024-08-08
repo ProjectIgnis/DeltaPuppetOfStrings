2c1d0dc015d7ac0e5be9d64d28818d6e856170ee
Naim <naimsantos45@gmail.com>
2024-08-08 13:40:29 +0000

    "Sinister Shadow Games" fix
    Fixed a bug in which it would only be able to flip up to 5 monsters from face-down to face-up

---
+++ script/official/c77505534.lua
@@ -0,0 +1,39 @@
+--堕ち影の蠢き
+--Sinister Shadow Games
+local s,id=GetID()
+function s.initial_effect(c)
+	--Send 1 "Shaddoll" card from deck to GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_POSITION)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_ATTACK+TIMING_BATTLE_START)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_series={SET_SHADDOLL}
+function s.filter(c)
+	return c:IsSetCard(SET_SHADDOLL) and c:IsAbleToGrave()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.posfilter(c)
+	return c:IsFacedown() and c:IsSetCard(SET_SHADDOLL)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 and g:GetFirst():IsLocation(LOCATION_GRAVE) then
+		local tg=Duel.GetMatchingGroup(s.posfilter,tp,LOCATION_MZONE,0,nil)
+		if #tg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+			local sg=tg:Select(tp,1,#tg,nil)
+			Duel.ChangePosition(sg,POS_FACEUP_DEFENSE)
+		end
+	end
+end

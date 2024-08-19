b2d957dc2132bca296438e3b8804aaadbb661b7e
Edoardo Lolletti <edoardo762@gmail.com>
2024-08-19 17:00:04 +0000

    Simplify logic in Red-Eyes Fusion and Ultimate Fusion

---
+++ script/official/c6172122.lua
@@ -0,0 +1,55 @@
+--真紅眼融合
+--Red-Eyes Fusion
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.ListsArchetypeAsMaterial,SET_RED_EYES),
+									 extrafil=s.fextra,stage2=s.stage2,extratg=s.extratg})
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(s.cost)
+	c:RegisterEffect(e1)
+end
+s.listed_names={CARD_REDEYES_B_DRAGON}
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetActivityCount(tp,ACTIVITY_SUMMON)==0 and Duel.GetActivityCount(tp,ACTIVITY_SPSUMMON)==0 end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SUMMON)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetTargetRange(1,0)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetLabelObject(e)
+	e2:SetTarget(s.splimit)
+	Duel.RegisterEffect(e2,tp)
+end
+function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	return se~=e:GetLabelObject()
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,0,tp,LOCATION_DECK)
+end
+function s.fextra(e,tp,mg,sumtype)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToGrave),tp,LOCATION_DECK,0,nil),s.fcheck
+end
+function s.stage2(e,tc,tp,mg,chk)
+	if chk==1 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_CODE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(CARD_REDEYES_B_DRAGON)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+end
+function s.fcheck(tp,sg,fc,sumtype,tp)
+	return sg:IsExists(s.ffilter,1,nil,fc,sumtype,tp)
+end
+function s.ffilter(c,fc,sumtype,tp)
+	return c:IsSetCard(SET_RED_EYES,fc,sumtype,tp) and (not fc.material or c:IsSummonCode(fc,sumtype,tp,fc.material))
+end

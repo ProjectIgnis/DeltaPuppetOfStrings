cfea312287c674ce32619bbe23dd8a0e1d0a1289
Naim <naimsantos45@gmail.com>
2024-10-29 22:58:35 +0000

    "Destruction Sword Memories" update
    - It was missing categories for the Fusion Summon effect, which caused incorrect interactions with effects that detect those (e.g. "Dominus Impulse")
    - script modernization

---
+++ script/official/c32104431.lua
@@ -0,0 +1,69 @@
+--破壊剣の追憶
+--Destruction Sword Memories
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon 1 "Buster Blader" monster from your Deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--Fusion Summon 1 "Buster Blader, the Dragon Destroyer Swordsman"
+	local params = {fusfilter=aux.FilterBoolFunction(Card.IsCode,86240887),matfilter=s.matfilter,extrafil=s.fextra,extraop=Fusion.BanishMaterial,extratg=s.extratg}
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON+CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+	e2:SetCost(aux.selfbanishcost)
+	e2:SetTarget(Fusion.SummonEffTG(params))
+	e2:SetOperation(Fusion.SummonEffOP(params))
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_DESTRUCTION_SWORD,SET_BUSTER_BLADER}
+s.listed_names={86240887} --"Buster Blader, the Dragon Destroyer Swordsman"
+function s.costfilter(c)
+	return c:IsSetCard(SET_DESTRUCTION_SWORD) and c:IsDiscardable()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(SET_BUSTER_BLADER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.matfilter(c)
+	return aux.SpElimFilter(c) and c:IsAbleToRemove()
+end
+function s.extrafil(c)
+	return c:IsMonster() and c:IsAbleToRemove()
+end
+function s.fextra(e,tp,mg)
+	if not Duel.IsPlayerAffectedByEffect(tp,CARD_SPIRIT_ELIMINATION) then
+		return Duel.GetMatchingGroup(s.extrafil,tp,LOCATION_GRAVE,0,nil)
+	end
+	return nil
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,LOCATION_GRAVE)
+end

bf1f55d329e966d38253dc91a9a572b082c17879
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2024-11-02 03:13:28 +0000

    Fix "Dark Contract with the Eternal Darkness (Anime)"
    Should be able to Tribute for a Special Summon/general script polish

---
+++ script/unofficial/c511247000.lua
@@ -0,0 +1,71 @@
+--常闇の契約書 (Anime)
+--Dark Contract with the Eternal Darkness (Anime)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_DRAW_PHASE)
+	c:RegisterEffect(e1)
+	--Spell/Traps cannot be activated that target a monster(s)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
+	e2:SetCondition(function(e) return not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,id),e:GetHandlerPlayer(),0,LOCATION_SZONE,1,nil) end)
+	e2:SetValue(s.evalue)
+	c:RegisterEffect(e2)
+	--Monsters cannot be Tributed for a Tribute Summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e3:SetCode(EFFECT_UNRELEASABLE_SUM)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetCondition(function(e) return not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,id),e:GetHandlerPlayer(),0,LOCATION_SZONE,1,nil) end)
+	e3:SetValue(s.sumlimit)
+	c:RegisterEffect(e3)
+	--Monsters cannot be material for a Fusion, Synchro, or Xyz Summon
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_CANNOT_BE_MATERIAL)
+	e4:SetValue(aux.AND(s.matlimit,aux.cannotmatfilter(SUMMON_TYPE_FUSION,SUMMON_TYPE_SYNCHRO,SUMMON_TYPE_XYZ)))
+	c:RegisterEffect(e4)
+	--Take 1000 damage during your Standby Phase
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,0))
+	e5:SetCategory(CATEGORY_DAMAGE)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e5:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCountLimit(1)
+	e5:SetCondition(function(e,tp) return Duel.GetTurnPlayer()==tp end)
+	e5:SetTarget(s.damtg)
+	e5:SetOperation(s.damop)
+	c:RegisterEffect(e5)
+end
+s.listed_names={id}
+function s.evalue(e,re,rp)
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and rp~=e:GetHandlerPlayer()
+end
+function s.matlimit(e,c,sumtype,tp)
+	if tp==PLAYER_NONE then tp=c:GetControler() end
+	return e:GetHandlerPlayer()==1-tp
+end
+function s.sumlimit(e,c)
+	if not c then return false end
+	return not c:IsControler(e:GetHandlerPlayer())
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1000)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,0,0,tp,1000)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end

31e7fb372fae2f750d3a0238b9898970ba6c2bf4
pyrQ <30827955+pyrQ@users.noreply.github.com>
2024-11-06 08:43:36 +0000

    "Megalith" fixes + Ritual proc update
    - Level 4 "Megalith" monsters: Their Ritual Summoning effect should resolve doing nothing if they have left the Monster Zone or if the opponent controls them when the effect resolves.
    - Level 8 "Megalith" monsters: Should be able to choose to Ritual Summon the same copy that was discarded as cost, or Tribute it for the Ritual Summon, if that card is in the hand/field when the effect resolves.
    
    Also includes minor fixes and general script updates.

---
+++ script/official/c63233638.lua
@@ -0,0 +1,36 @@
+--メガリス・ファレグ
+--Megalith Phaleg
+--Scripted by ahtelel
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	local ritual_target_params={handler=c,lvtype=RITPROC_GREATER,filter=function(ritual_c) return ritual_c:IsSetCard(SET_MEGALITH) and ritual_c~=c end,forcedselection=s.forcedselection}
+	local ritual_operation_params={handler=c,lvtype=RITPROC_GREATER,filter=function(ritual_c) return ritual_c:IsSetCard(SET_MEGALITH) end}
+	--Ritual Summon 1 "Megalith" Ritual Monster from your hand, by Tributing monsters from your hand or field whose total Levels equal or exceed its Level
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_RELEASE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(aux.SelfDiscardCost)
+	e1:SetTarget(Ritual.Target(ritual_target_params))
+	e1:SetOperation(Ritual.Operation(ritual_operation_params))
+	c:RegisterEffect(e1)
+	--Monsters you control gain 300 ATK/DEF for each Ritual Monster in your GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetValue(function(e,c) return 300*Duel.GetMatchingGroupCount(Card.IsRitualMonster,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil) end)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e3)
+end
+s.listed_series={SET_MEGALITH}
+function s.forcedselection(e,tp,g,sc)
+	local c=e:GetHandler()
+	return not g:IsContains(c),g:IsContains(c)
+end
---
+++ script/official/c99628747.lua
@@ -0,0 +1,51 @@
+--メガリス・ベトール
+--Megalith Bethor
+--Scripted by AlphaKretin
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	local ritual_target_params={handler=c,lvtype=RITPROC_GREATER,filter=function(ritual_c) return ritual_c:IsSetCard(SET_MEGALITH) and ritual_c~=c end,forcedselection=s.forcedselection}
+	local ritual_operation_params={handler=c,lvtype=RITPROC_GREATER,filter=function(ritual_c) return ritual_c:IsSetCard(SET_MEGALITH) end}
+	--Ritual Summon 1 "Megalith" Ritual Monster from your hand, by Tributing monsters from your hand or field whose total Levels equal or exceed its Level
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_RELEASE+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(aux.SelfDiscardCost)
+	e1:SetTarget(Ritual.Target(ritual_target_params))
+	e1:SetOperation(Ritual.Operation(ritual_operation_params))
+	c:RegisterEffect(e1)
+	--Destroy cards your opponent controls, up to the number of Ritual Monsters with different names in your GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(function(e) return e:GetHandler():IsRitualSummoned() end)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_MEGALITH}
+function s.forcedselection(e,tp,g,sc)
+	local c=e:GetHandler()
+	return not g:IsContains(c),g:IsContains(c)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() end
+	local ct=Duel.GetMatchingGroup(Card.IsRitualMonster,tp,LOCATION_GRAVE,0,nil):GetClassCount(Card.GetCode)
+	if chk==0 then return ct>0 and Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_ONFIELD,1,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,tp,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetTargetCards(e)
+	if #tg>0 then
+		Duel.Destroy(tg,REASON_EFFECT)
+	end
+end

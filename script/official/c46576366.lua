4f6a7cb523518a7c48a227d2c1f5363c92aca946
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-01-09 03:11:11 +0000

    Fix "Lyrilusc - Celestine Wagtail"
    Missing chkc information/slight script modernization

---
+++ script/official/c46576366.lua
@@ -0,0 +1,60 @@
+--ＬＬ－セレスト・ワグテイル
+--Lyrilusc - Celestine Wagtail
+--Scripted by The Razgriz
+local s,id=GetID()
+function s.initial_effect(c)
+	--Add 1 "Lyrilusc" Spell/Trap from your Deck to your hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--Attach this card from your GY to 1 "Lyrilusc" Xyz monster you control
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_LEAVE_GRAVE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.atchtg)
+	e2:SetOperation(s.atchop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_LYRILUSC}
+function s.thfilter(c)
+	return c:IsSetCard(SET_LYRILUSC) and c:IsSpellTrap() and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.atchfilter(c)
+	return c:IsFaceup() and c:IsSetCard(SET_LYRILUSC) and c:IsType(TYPE_XYZ)
+end
+function s.atchtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.atchfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.atchfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.atchfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
+end
+function s.atchop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+		Duel.Overlay(tc,c)
+	end
+end

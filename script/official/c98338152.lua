03aa892c60f8ef116bc6e1cfb13bdec07c55aa4f
Naim <naimsantos45@gmail.com>
2025-01-20 20:56:40 +0000

    "Sky Striker Mecha - Widow Anchor" fix
    Fixed a bug where it would not be able to take control of a monster that had an effect to prevent control changes, even after the effect was negated

---
+++ script/official/c98338152.lua
@@ -0,0 +1,45 @@
+--閃刀機－ウィドウアンカー
+--Sky Striker Mecha - Widow Anchor
+--Scripted by ahtelel
+local s,id=GetID()
+function s.initial_effect(c)
+	--Negate the effects of a face-up monster on the field
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsExistingMatchingCard(Card.IsInMainMZone,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and not c:IsDisabled()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	if Duel.GetMatchingGroupCount(Card.IsSpell,tp,LOCATION_GRAVE,0,nil)>=3 then
+		e:SetCategory(CATEGORY_DISABLE|CATEGORY_CONTROL)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsDisabled() then
+		tc:NegateEffects(c,RESET_PHASE|PHASE_END,true)
+		Duel.AdjustInstantly(tc)
+		if Duel.GetMatchingGroupCount(Card.IsSpell,tp,LOCATION_GRAVE,0,nil)>=3 and tc:IsControlerCanBeChanged()
+			and tc:IsControler(1-tp) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.BreakEffect()
+			Duel.GetControl(tc,tp,PHASE_END,1)
+		end
+	end
+end

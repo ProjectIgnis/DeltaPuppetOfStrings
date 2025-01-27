212a5094cb858390ef2bef2803de79912bd0a180
Naim <naimsantos45@gmail.com>
2025-01-27 12:46:57 +0000

    "Rare Metalmorph" fix
    - fixed a bug where it would be able to negate the effect of card that was already targetting the monster before this card's activation such as Equip Spell

---
+++ script/official/c12503902.lua
@@ -0,0 +1,61 @@
+--レアメタル化・魔法反射装甲
+--Rare Metalmorph
+local s,id=GetID()
+function s.initial_effect(c)
+	--Equip procedure
+	aux.AddPersistentProcedure(c,0,s.filter,CATEGORY_ATKCHANGE,EFFECT_FLAG_DAMAGE_STEP,nil,TIMING_DAMAGE_STEP,s.condition,nil,nil,s.operation)
+	--Increase ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(aux.PersistentTargetFilter)
+	e1:SetValue(500)
+	c:RegisterEffect(e1)
+	--Destroy this card when the monster leaves the field
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetCondition(s.selfdescond)
+	e2:SetOperation(s.selfdesop)
+	c:RegisterEffect(e2)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated()
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsRace(RACE_MACHINE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and s.filter(tc) and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetRange(LOCATION_SZONE)
+		e1:SetCode(EVENT_CHAIN_SOLVING)
+		e1:SetCondition(s.discon)
+		e1:SetOperation(s.disop)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		c:RegisterEffect(e1,true)
+	end
+end
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetHandler():GetFirstCardTarget()
+	if not tc or not re:IsActiveType(TYPE_SPELL) or not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	return g:IsContains(tc)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateEffect(ev)
+	e:Reset()
+end
+function s.selfdescond(e,tp,eg,ep,ev,re,r,rp)
+	local tc=e:GetHandler():GetFirstCardTarget()
+	return tc and eg:IsContains(tc)
+end
+function s.selfdesop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+end

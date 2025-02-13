f9d3d55426ddaa0dcbd3bfe5643f6631b6c4118a
Naim <naimsantos45@gmail.com>
2025-02-13 07:36:44 +0000

    Some card fixes
    - fixed a bug with some "Archfiend" cards that would be able to negate an effect that targets an "Archfiend" and/or themselves even when the targeted changed locations from the moment the effect was activated to the moment it resolved
    - cards changed: Skull Archfiend of Lightning, Shadowknight Archfiend, Terrorking Archfiend, Infernalqueen Archfiend, Darkbishop Archfiend, Vilepawn Archfiend

---
+++ script/official/c35798491.lua
@@ -0,0 +1,48 @@
+--ダークビショップデーモン
+--Darkbishop Archfiend
+local s,id=GetID()
+function s.initial_effect(c)
+	--Once per turn, during your Standby Phase, you must pay 500 LP (this is not optional), or this card is destroyed
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(function(e,tp) return Duel.IsTurnPlayer(tp) end)
+	e1:SetOperation(s.mtop)
+	c:RegisterEffect(e1)
+	--Roll a six-sided die when resolving an opponent's card effect that targets an "Archfiend" monster you control
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_CHAIN_SOLVING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(s.disop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_ARCHFIEND}
+s.roll_dice=true
+function s.mtop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.CheckLPCost(tp,500) then
+		Duel.PayLPCost(tp,500)
+	else
+		Duel.Destroy(e:GetHandler(),REASON_COST)
+	end
+end
+function s.filter(c,tp,re)
+	return c:IsSetCard(SET_ARCHFIEND) and c:IsRelateToEffect(re) and c:IsLocation(LOCATION_MZONE)
+		and c:IsControler(tp) and c:IsFaceup()
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	if not (re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) and ep==1-tp) then return false end
+	local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	if not tg or not tg:IsExists(s.filter,1,nil,tp,re) or not Duel.IsChainDisablable(ev) then return false end
+	local rc=re:GetHandler()
+	Duel.Hint(HINT_CARD,1-tp,id)
+	local dc=Duel.TossDice(tp,1)
+	if dc==1 or dc==3 or dc==6 then
+		if Duel.NegateEffect(ev) and rc:IsRelateToEffect(re) then
+			Duel.Destroy(rc,REASON_EFFECT)
+		end
+	end
+end
---
+++ script/official/c8581705.lua
@@ -0,0 +1,79 @@
+--インフェルノクインデーモン
+--Infernalqueen Archfiend
+local s,id=GetID()
+function s.initial_effect(c)
+	--Once per turn, during your Standby Phase, you must pay 500 LP (this is not optional), or this card is destroyed
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(function(e,tp) return Duel.IsTurnPlayer(tp) end)
+	e1:SetOperation(s.mtop)
+	c:RegisterEffect(e1)
+	--Roll a six-sided die when resolving an opponent's card effect that targets this card
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_CHAIN_SOLVING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetOperation(s.disop)
+	c:RegisterEffect(e2)
+	--Increase the ATK of one "Archfiend" monster by 1000
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetTarget(s.atktg)
+	e3:SetOperation(s.atkop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={SET_ARCHFIEND}
+s.roll_dice=true
+function s.mtop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.CheckLPCost(tp,500) then
+		Duel.PayLPCost(tp,500)
+	else
+		Duel.Destroy(e:GetHandler(),REASON_COST)
+	end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local c=e:GetHandler()
+	if not (c:IsRelateToEffect(re) and ep==1-tp) then return end
+	local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	if not tg or not tg:IsContains(c) or not Duel.IsChainDisablable(ev) then return false end
+	local rc=re:GetHandler()
+	Duel.Hint(HINT_CARD,1-tp,id)
+	local dc=Duel.TossDice(tp,1)
+	if dc~=2 and dc~=5 then return end
+	if Duel.NegateEffect(ev) and rc:IsRelateToEffect(re) then
+		Duel.Destroy(rc,REASON_EFFECT)
+	end
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(SET_ARCHFIEND)
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,g,1,tp,1000)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(1000)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end

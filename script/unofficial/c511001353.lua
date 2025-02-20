2282c3608c9e96a4e17379a331c1fcacf1befaf7
TheRazgriz <54089012+TheRazgriz@users.noreply.github.com>
2025-02-20 04:26:51 +0000

    Update "Wild Half"
    - Should halve the current ATK of the monster
    - Cleaned up the monster attribute addition for the "Half Token"
    - Checked for level during resolution in the cases of the effect target being switched (eg by the effect of 'Cairngorgon')
    - General script modernization updates

---
+++ script/unofficial/c511001353.lua
@@ -0,0 +1,81 @@
+--ワイルド・ハーフ
+--Wild Half
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(function(e) return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,86188410),e:GetHandlerPlayer(),LOCATION_ONFIELD,0,1,nil) end)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+s.listed_names={86188410,id+1} --"Elemental HERO Wildheart","Half Token"
+function s.tknspfilter(c,tp)
+	return c:IsFaceup() and c:HasLevel()
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,c:GetAttack(),c:GetDefense(),c:GetLevel(),c:GetRace(),c:GetAttribute(),POS_FACEUP,1-tp)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.filter(chkc,tp) end
+	if chk==0 then return Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0 
+		and Duel.IsExistingTarget(s.tknspfilter,tp,0,LOCATION_MZONE,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.tknspfilter,tp,0,LOCATION_MZONE,1,1,nil,tp)
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not tc or not tc:IsRelateToEffect(e) or tc:IsFacedown() then return end
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_SET_ATTACK_FINAL)
+	e0:SetReset(RESET_EVENT|RESETS_STANDARD)
+	e0:SetValue(math.ceil(tc:GetAttack()/2))
+	tc:RegisterEffect(e0)
+	if tc:IsImmuneToEffect(e0) or not tc:HasLevel() or Duel.GetLocationCount(1-tp,LOCATION_MZONE)<=0
+		or not Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0,TYPES_TOKEN,tc:GetAttack(),tc:GetDefense(),
+			tc:GetLevel(),tc:GetRace(),tc:GetAttribute(),POS_FACEUP,1-tp) then return end
+	Duel.BreakEffect()
+	local token=Duel.CreateToken(tp,id+1)
+	--Add monster properties
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SET_BASE_ATTACK)
+	e1:SetValue(tc:GetAttack())
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD&~RESET_TOFIELD)
+	token:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_SET_BASE_DEFENSE)
+	e2:SetValue(tc:GetDefense())
+	token:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EFFECT_CHANGE_LEVEL)
+	e3:SetValue(tc:GetLevel())
+	token:RegisterEffect(e3)
+	local e4=e1:Clone()
+	e4:SetCode(EFFECT_CHANGE_RACE)
+	e4:SetValue(tc:GetRace())
+	token:RegisterEffect(e4)
+	local e5=e1:Clone()
+	e5:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+	e5:SetValue(tc:GetAttribute())
+	token:RegisterEffect(e5)
+	if Duel.SpecialSummonStep(token,0,tp,1-tp,false,false,POS_FACEUP) then
+		--This token gains the effects of the targeted monster
+		if tc:IsOriginalType(TYPE_EFFECT) then
+			token:CopyEffect(tc:GetOriginalCode(),RESET_EVENT|RESETS_STANDARD)
+			local e6=Effect.CreateEffect(c)
+			e6:SetType(EFFECT_TYPE_SINGLE)
+			e6:SetCode(EFFECT_ADD_TYPE)
+			e6:SetValue(TYPE_EFFECT)
+			e6:SetReset(RESET_EVENT|RESETS_STANDARD)
+			token:RegisterEffect(e6)
+		end
+	end
+	Duel.SpecialSummonComplete()
+end
